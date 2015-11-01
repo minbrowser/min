9745fa562e5161f4d3ff625a5c4a1f840ee96971
PalmerAL <PalmerAL@users.noreply.github.com>
2015-11-01 09:42:07 -0600

    improve protocol support

--- js/urlParser.js
+++ js/urlParser.js
@@ -1,14 +1,14 @@
 var urlParser = {
 	searchBaseURL: "https://duckduckgo.com/?q=%s",
 	isUrl: function (url) {
-		return url.indexOf("http://") == 0 || url.indexOf("https://") == 0 || url.indexOf("about:") == 0 || url.indexOf("chrome:") == 0 || url.indexOf("file://") == 0;
+		return url.indexOf("http://") == 0 || url.indexOf("https://") == 0 || url.indexOf("file://") == 0 || url.indexOf("about:") == 0 || url.indexOf("chrome:") == 0 || url.indexOf("data:") == 0;
 	},
 	removeProtocol: function (url) {
 		if (!urlParser.isUrl(url)) {
 			return url;
 		}
 
-		var withoutProtocol = url.replace("http://", "").replace("https://", "").replace("file://", "").replace("chrome:", "");
+		var withoutProtocol = url.replace("http://", "").replace("https://", "").replace("file://", ""); //chrome:, about:, data: protocols intentionally not removed
 
 		if (withoutProtocol.indexOf("www.") == 0) {
 			var final = withoutProtocol.replace("www.", "");
