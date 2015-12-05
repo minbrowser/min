237ec591db6a5c608ce1601669bbe36d447be960
PalmerAL <PalmerAL@users.noreply.github.com>
2015-12-05 12:32:24 -0600

    bugfix

--- js/fileDownloadManager.js
+++ js/fileDownloadManager.js
@@ -2,21 +2,22 @@
 
 var PDFViewerURL = "file://" + __dirname + "/pdfjs/web/viewer.html?url=";
 
-ipc.on("openPDF", function (data) {
-	console.log(data);
+ipc.on("openPDF", function (event, filedata) {
+	console.log(filedata);
 	var cTab = tabs.get(tabs.getSelected());
+
 	var webview = getWebview(cTab.id);
 
 	//If the current tab is blank or has the url of the pdf we are opening, we open the pdf in the current tab. Otherwise, to avoid losing pages, we open a new tab with the pdf.
 
-	var PDFurl = PDFViewerURL + data.url;
+	var PDFurl = PDFViewerURL + filedata.url;
 
 	if (cTab.url == PDFurl) { //if we are already on the pdf we are navigating to, ignore it
 		return;
 	}
 
 
-	if (cTab.url == "about:blank" || cTab.url == data.item.url) {
+	if (cTab.url == "about:blank" || cTab.url == filedata.item.url) {
 		navigate(tabs.getSelected(), PDFurl)
 	} else {
 
