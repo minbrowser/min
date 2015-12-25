8eb27f45ba8ee86bdb973b83e0a7f8ca460cb91d
PalmerAL <PalmerAL@users.noreply.github.com>
2015-12-25 15:51:53 -0600

    bug fix

--- js/searchbar/searchbar.js
+++ js/searchbar/searchbar.js
@@ -75,7 +75,7 @@ function openURLInBackground(url) { //used to open a url in the background, with
 	var newTab = tabs.add({
 		url: url,
 		private: tabs.get(tabs.getSelected()).private
-	})
+	}, tabs.getIndex(tabs.getSelected()) + 1);
 	addTab(newTab, {
 		focus: false,
 		openInBackground: true,
