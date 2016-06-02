a0bc96e3ec637f3a518cb3f768e18aff70d38625
PalmerAL <PalmerAL@users.noreply.github.com>
2016-06-02 15:29:53 -0500

    add some custom !bangs

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,7 @@ module.exports = function (grunt) {
 							"js/searchbar/bookmarksPlugin.js",
 							"js/searchbar/openTabsPlugin.js",
 							"js/searchbar/bangsPlugin.js",
+							"js/searchbar/customBangs.js",
 							"js/searchbar/searchSuggestionsPlugin.js",
 							"js/searchbar/historySuggestionsPlugin.js",
 							"js/searchbar/hostsSuggestionsPlugin.js",
