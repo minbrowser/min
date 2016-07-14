431fb93cb4e51e9249be7f0e4eb59cd4aee2b2b1
PalmerAL <PalmerAL@users.noreply.github.com>
2016-07-14 14:28:38 -0500

    move places search into a separate file

--- js/bookmarksHistory/placesWorker.js
+++ js/bookmarksHistory/placesWorker.js
@@ -4,6 +4,7 @@ importScripts('../../node_modules/dexie/dist/dexie.min.js')
 importScripts('../../node_modules/string_score/string_score.min.js')
 importScripts('../util/database.js')
 importScripts('fullTextSearch.js')
+importScripts('placesSearch.js')
 
 var spacesRegex = /[\+\s._/-]/g // things that could be considered spaces
 var wordRegex = /^[a-z\s]+$/g
@@ -164,123 +165,12 @@ onmessage = function (e) {
   }
 
   if (action === 'searchPlaces') { // do a history search
-    function processItem (item) {
-      // if the text does not contain the first search word, it can't possibly be a match, so don't do any processing
-      var itext = item.url.split('?')[0].replace('http://', '').replace('https://', '').replace('www.', '')
-
-      if (item.url !== item.title) {
-        itext += ' ' + item.title
-      }
-
-      itext = itext.toLowerCase().replace(spacesRegex, ' ')
-
-      var tindex = itext.indexOf(st)
-
-      // if the url contains the search string, count as a match
-      // prioritize matches near the beginning of the url
-      if (tindex === 0) {
-        item.boost = itemStartBoost // large amount of boost for this
-        matches.push(item)
-      } else if (tindex !== -1) {
-        item.boost = exactMatchBoost
-        matches.push(item)
-      } else {
-        // if all of the search words (split by spaces, etc) exist in the url, count it as a match, even if they are out of order
-
-        if (substringSearchEnabled) {
-          var substringMatch = true
-
-          // check if the search text matches but is out of order
-          for (var i = 0; i < searchWords.length; i++) {
-            if (itext.indexOf(searchWords[i]) === -1) {
-              substringMatch = false
-              break
-            }
-          }
-
-          if (substringMatch) {
-            item.boost = 0.125 * searchWords.length + (0.02 * stl)
-            matches.push(item)
-            return
-          }
-        }
-
-        if (item.visitCount !== 1 || item.lastVisit > oneWeekAgo) { // if the item has been visited more than once, or has been visited in the last week, we should calculate the fuzzy score. Otherwise, it is ignored. This reduces the number of bad results and increases search speed.
-          var score = itext.score(st, 0)
-
-          if (score > 0.4 + (0.00075 * itext.length)) {
-            item.boost = score * 0.5
-
-            if (score > 0.62) {
-              item.boost += 0.33
-            }
-
-            matches.push(item)
-          }
-        }
-      }
-    }
-
-    var tstart = performance.now()
-    var matches = []
-    var st = searchText.replace(spacesRegex, ' ').split('?')[0].replace('http://', '').replace('https://', '').replace('www.', '')
-    var stl = searchText.length
-    var searchWords = st.split(' ')
-    var substringSearchEnabled = false
-    var itemStartBoost = Math.min(2.5 * stl, 10)
-    var exactMatchBoost = 0.4 + (0.075 * stl)
-
-    if (searchText.indexOf(' ') !== -1) {
-      substringSearchEnabled = true
-    }
-
-    for (var i = 0; i < historyInMemoryCache.length; i++) {
-      if (matches.length > 200) {
-        break
-      }
-      processItem(historyInMemoryCache[i])
-    }
-
-    function afterSearchDone () {
-      matches.sort(function (a, b) { // we have to re-sort to account for the boosts applied to the items
-        return calculateHistoryScore(b) - calculateHistoryScore(a)
-      })
-
-      var tend = performance.now()
-
-      console.info('history search took', tend - tstart)
+    searchPlaces(searchText, function (matches) {
       postMessage({
-        result: matches.slice(0, 100),
+        result: matches,
         scope: 'history'
       })
-    }
-
-    // if there are not enough matches, do a full-text search
-
-    if (matches.length < 4 && doneLoadingHistoryCache) {
-      fullTextPlacesSearch(searchText, function (searchResults) {
-        // add the full-text matches to the title and URL matches
-        matches = matches.concat(searchResults)
-
-        // remove duplicates
-
-        var urlSet = []
-
-        for (var i = 0; i < matches.length; i++) {
-          // if the item has already been found in the array, remove it
-          if (urlSet.indexOf(matches[i].url) !== -1) {
-            matches.splice(i, 1)
-            i--
-          } else {
-            urlSet.push(matches[i].url)
-          }
-        }
-
-        afterSearchDone()
-      })
-    } else {
-      afterSearchDone()
-    }
+    })
   }
 
   if (action === 'getPlaceSuggestions') {
