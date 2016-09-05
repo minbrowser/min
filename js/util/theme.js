4c62b7544ab1330b328e1de35c80eb5b05d675c3
Pandawan <pandawanfr@gmail.com>
2016-09-04 18:06:06 -0700

    Task List now has Dark Mode!
    Can't figure out how to make the top bar have a dark mode so...

--- js/util/theme.js
+++ js/util/theme.js
@@ -2,7 +2,13 @@ var hours = new Date().getHours();
 
 settings.get('darkMode', function(value) {
   if (value === true || (hours > 21 || hours < 6)) {
-    document.body.classList.add('dark-mode')
+    if (document.body.contains(document.getElementById('browser-view'))) {
+      document.getElementById('task-overlay').classList.add('dark-mode')
+      document.getElementById('top-bar').classList.add('dark-mode')
+    }
+    else {
+      document.body.classList.add('dark-mode')
+    }
     window.isDarkMode = true
   }
 })
