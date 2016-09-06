1361d56ff28d802fac7d22bd40c1d807a0e21df7
Pandawan <pandawanfr@gmail.com>
2016-09-06 16:33:54 -0700

    FIxed some stuff as requested by PalmerAl
    Note: Some of the fixes didn't completely work so I had to change a bit of stuff, which might not look as clean as you would want it

--- js/util/theme.js
+++ js/util/theme.js
@@ -2,13 +2,7 @@ var hours = new Date().getHours();
 
 settings.get('darkMode', function(value) {
   if (value === true || (hours > 21 || hours < 6)) {
-    if (document.body.contains(document.getElementById('browser-view'))) {
-      document.getElementById('task-overlay').classList.add('dark-mode')
-      document.getElementById('top-bar').classList.add('dark-mode')
-    }
-    else {
       document.body.classList.add('dark-mode')
-    }
     window.isDarkMode = true
   }
 })
