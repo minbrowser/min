06019be09cf916ad33764a918b1839d024589106
PalmerAL <PalmerAL@users.noreply.github.com>
2016-09-18 20:47:37 -0500

    switch to dark mode when the time changes

--- js/util/theme.js
+++ js/util/theme.js
@@ -1,8 +1,21 @@
-var hours = new Date().getHours();
+function darkModeEnabled () {
+  var hours = new Date().getHours()
+  return hours > 21 || hours < 6
+}
 
-settings.get('darkMode', function(value) {
-  if (value === true || (hours > 21 || hours < 6)) {
-      document.body.classList.add('dark-mode')
+settings.get('darkMode', function (value) {
+  if (value === true || darkModeEnabled()) {
+    document.body.classList.add('dark-mode')
     window.isDarkMode = true
+  } else {
+    setInterval(function () {
+      if (darkModeEnabled()) {
+        document.body.classList.add('dark-mode')
+        window.isDarkMode = true
+      } else {
+        document.body.classList.remove('dark-mode')
+        window.isDarkMode = false
+      }
+    }, 10000)
   }
 })
