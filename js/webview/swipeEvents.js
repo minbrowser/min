a831924dee47d4a5113320a42e033078771c7e42
PalmerAL <PalmerAL@users.noreply.github.com>
2016-10-22 14:01:23 -0500

    Revert "redesign the swipe gestures"
    This reverts commit c5d63d4905ae63607e4937afb67110c30b16c460.

--- js/webview/swipeEvents.js
+++ js/webview/swipeEvents.js
@@ -1,58 +1,13 @@
-// https://remysharp.com/2010/07/21/throttling-function-calls
+/* detects back/forward swipes */
 
-function debounce (fn, delay) {
-  var timer = null
-  return function () {
-    var context = this
-    var args = arguments
-    clearTimeout(timer)
-    timer = setTimeout(function () {
-      fn.apply(context, args)
-    }, delay)
-  }
-}
+var totalMouseMove = 0
+var verticalMouseMove = 0
+var eventsCaptured = 0
+var documentUnloaded = false
 
-var horizontalMouseMove = 0;
-var verticalMouseMove = 0;
-
-function resetCounters() {
-    horizontalMouseMove = 0;
-    verticalMouseMove = 0;
-}
-
-var onSwipeGestureFinish = debounce(function () {
-
-    //swipe to the left to go forward
-    if(horizontalMouseMove > 150 && Math.abs(horizontalMouseMove / verticalMouseMove) > 2.5 && document.scrollingElement.scrollLeft + document.scrollingElement.clientWidth == document.scrollingElement.scrollWidth) {
-
-      //if the page can be scrolled, increase the minimum swipe distance
-      if(document.scrollingElement.scrollWidth === document.scrollingElement.clientWidth || horizontalMouseMove > 800) {
-        resetCounters();
-        window.history.go(1)
-      }
-    }
-
-    //swipe to the right to go backwards
-    if(horizontalMouseMove < -150 && Math.abs(horizontalMouseMove / verticalMouseMove) > 2.5 && document.scrollingElement.scrollLeft === 0) {
-      
-      if(document.scrollingElement.scrollWidth === document.scrollingElement.clientWidth || horizontalMouseMove < -800) {
-        resetCounters();
-       window.history.go(-1)
-      }
-    }
-
-    resetCounters();
-}, 70);
-
-window.addEventListener('wheel', function(e) {
-
-    horizontalMouseMove += e.deltaX
-    verticalMouseMove += e.deltaY
-
-    if(Math.abs(e.deltaX) >= 20 || Math.abs(e.deltaY) >= 20) {
-        onSwipeGestureFinish();
-    }
-
+window.addEventListener('mousewheel', function (e) {
+  verticalMouseMove += e.deltaY
+  eventsCaptured++
   /* default zoom modifier is ctrl. Mac uses cmd/meta/super so an exeption will be made below */
   var platformZoomKey = e.ctrlKey
 
@@ -63,24 +18,54 @@ window.addEventListener('wheel', function(e) {
   if (navigator.platform === 'MacIntel') {
     if (e.ctrlKey && !e.defaultPrevented) {
       if (verticalMouseMove > 10) {
-       zoomOut()
+        return zoomOut()
       }
       if (verticalMouseMove < -10) {
-        zoomIn()
+        return zoomIn()
       }
     }
     platformZoomKey = e.metaKey
   }
   /* cmd-key while scrolling should zoom in and out */
 
-  if (verticalMouseMove > 55 && platformZoomKey) {
+  if (verticalMouseMove > 55 && platformZoomKey && eventsCaptured > 1) {
     verticalMouseMove = -10
-    zoomOut()
+    return zoomOut()
   }
 
-  if (verticalMouseMove < -55 && platformZoomKey) {
+  if (verticalMouseMove < -55 && platformZoomKey && eventsCaptured > 1) {
     verticalMouseMove = -10
-    zoomIn()
+    return zoomIn()
+  }
+  if (e.deltaY > 5 || e.deltaY < -10) {
+    return
   }
 
+  if (!documentUnloaded) {
+    totalMouseMove += e.deltaX
+
+    if (totalMouseMove < -150) {
+      doneNavigating = true
+      window.history.back()
+      documentUnloaded = true
+      setTimeout(function () {
+        documentUnloaded = false
+      }, 3000)
+    } else if (totalMouseMove > 100) {
+      documentUnloaded = true
+      window.history.go(1)
+      setTimeout(function () {
+        documentUnloaded = false
+      }, 3000)
+    }
+  }
 })
+
+setInterval(function () {
+  totalMouseMove = 0
+}, 4000)
+
+setInterval(function () {
+  verticalMouseMove = 0
+  eventsCaptured = 0
+}, 1000)
