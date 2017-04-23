ec8f7246f4740130dc60ac31bcc4f8f80dabac8c
PalmerAL <PalmerAL@users.noreply.github.com>
2017-04-23 15:39:34 -0500

    improvements to error pages

--- pages/error/error.js
+++ pages/error/error.js
@@ -1,100 +1,110 @@
+var searchParams = new URLSearchParams(window.location.search.replace('?', ''))
+
+var h1 = document.getElementById('error-name')
+var h2 = document.getElementById('error-desc')
+var primaryButton = document.getElementById('primary-button')
+var secondaryButton = document.getElementById('secondary-button')
+
+var ec = searchParams.get('ec')
+var url = searchParams.get('url')
+
 var websiteNotFound = {
-	name: "Server not found",
-	message: "Min couldn't find this website. If you think there should be something here, you can <a id='archivesearch-link'>search on archive.org</a> for this page."
+  name: 'Server not found',
+  message: "Min couldn't find this website.",
+  secondaryAction: {
+    title: 'Search on archive.org',
+    url: 'https://web.archive.org/web/*/' + url
+  }
 }
 
 var sslError = {
-	name: "This website is not available",
-	message: "Min couldn't connect securely to this website."
+  name: 'This website is not available',
+  message: "Min couldn't connect securely to this website."
 }
 
 var dnsError = {
-	name: "Website not found",
-	messge: "A DNS error occured."
+  name: 'Website not found',
+  messge: 'A DNS error occured.'
 }
 
 var offlineError = {
-	name: "You are offline",
-	message: "Reconnect to the internet and try again."
+  name: 'You are offline',
+  message: 'Reconnect to the internet and try again.'
 }
 
-//list: https://code.google.com/p/chromium/codesearch#chromium/src/net/base/net_error_list.h&sq=package:chromium&type=cs
+// list: https://code.google.com/p/chromium/codesearch#chromium/src/net/base/net_error_list.h&sq=package:chromium&type=cs
 const errorCodes = {
-	"-21": offlineError, //network changed
-	"-104": {
-		message: "Min couldn't connect to the website."
-	},
-	"-105": websiteNotFound,
-	"-106": offlineError,
-	"-107": sslError,
-	"-109": websiteNotFound,
-	"-110": sslError, //this is actually the error code for "server requested a client certificate", but we don't support that yet,
-	"-112": sslError,
-	"-113": sslError,
-	"-116": sslError,
-	"-117": sslError,
-	"-200": sslError,
-	"-201": {
-		name: "This website is not available",
-		message: "Min couldn't connect securely to this website. Please make sure your computer's clock is set correctly."
-	},
-	"-202": sslError,
-	"-203": sslError,
-	"-204": sslError,
-	"-205": sslError,
-	"-206": sslError,
-	"-207": sslError,
-	"-208": sslError,
-	"-210": sslError,
-	"-211": sslError,
-	"-212": sslError,
-	"-213": sslError,
-	"-300": {
-		name: "This address is invalid."
-	},
-	"-501": sslError,
-	"-800": dnsError,
-	"-801": dnsError,
-	"-802": dnsError,
-	"-803": dnsError,
-	"-804": dnsError,
-	"-805": dnsError,
-	"-806": dnsError,
+  '-21': offlineError, // network changed
+  '-104': {
+    message: "Min couldn't connect to the website."
+  },
+  '-105': websiteNotFound,
+  '-106': offlineError,
+  '-107': sslError,
+  '-109': websiteNotFound,
+  '-110': sslError, // this is actually the error code for "server requested a client certificate", but we don't support that yet,
+  '-112': sslError,
+  '-113': sslError,
+  '-116': sslError,
+  '-117': sslError,
+  '-200': sslError,
+  '-201': {
+    name: 'This website is not available',
+    message: "Min couldn't connect securely to this website. Please make sure your computer's clock is set correctly."
+  },
+  '-202': sslError,
+  '-203': sslError,
+  '-204': sslError,
+  '-205': sslError,
+  '-206': sslError,
+  '-207': sslError,
+  '-208': sslError,
+  '-210': sslError,
+  '-211': sslError,
+  '-212': sslError,
+  '-213': sslError,
+  '-300': {
+    name: 'This address is invalid.'
+  },
+  '-501': sslError,
+  '-800': dnsError,
+  '-801': dnsError,
+  '-802': dnsError,
+  '-803': dnsError,
+  '-804': dnsError,
+  '-805': dnsError,
+  '-806': dnsError
 }
 
-var searchParams = new URLSearchParams(window.location.search.replace("?", ""));
+// show the error message and detail
 
-//show the error message and detail
+var err = errorCodes[ec]
 
-var h1 = document.getElementById("error-name");
-var h2 = document.getElementById("error-desc");
-var button = document.getElementById("ta-button");
-
-var ec = searchParams.get("ec");
-var url = searchParams.get("url");
-
-if (errorCodes[ec]) {
-	h1.innerHTML += errorCodes[ec].name || "";
-	h2.innerHTML += errorCodes[ec].message || "";
+if (err) {
+  h1.innerHTML += err.name || ''
+  h2.innerHTML += err.message || ''
 } else {
-	h1.innerHTML += "An error occured";
+  h1.innerHTML += 'An error occured'
 }
 
-
-var link = document.getElementById("archivesearch-link");
-
-if (link) {
-	link.href = "https://web.archive.org/web/*/" + url;
+if (err.secondaryAction) {
+  secondaryButton.hidden = false
+  secondaryButton.textContent = err.secondaryAction.title
+  secondaryButton.addEventListener('click', function () {
+    window.location = err.secondaryAction.url
+  })
 }
 
-//if an ssl error occured, "try again" should go to the http:// version, which might work
+// if an ssl error occured, "try again" should go to the http:// version, which might work
 
-if (errorCodes[ec] == sslError) {
-	url = url.replace("https://", "http://");
+if (errorCodes[ec] === sslError) {
+  url = url.replace('https://', 'http://')
 }
 
 if (url) {
-	button.addEventListener("click", function () {
-		window.location = url;
-	});
+  primaryButton.addEventListener('click', function () {
+    window.location = url
+  })
 }
+
+primaryButton.focus()
--- pages/phishing/phishingError.js
+++ pages/phishing/phishingError.js
@@ -1,34 +1,34 @@
-var ignoreButton = document.getElementById("ignore-button");
-var continueButton = document.getElementById("continue-button");
+var ignoreButton = document.getElementById('ignore-button')
+var continueButton = document.getElementById('continue-button')
 
-ignoreButton.addEventListener("click", function () {
-	//add the domain to the exception list
+ignoreButton.addEventListener('click', function () {
+    // add the domain to the exception list
 
-	settings.get("phishingWhitelist", function (value) {
-		if (!value) {
-			value = [];
-		}
+  settings.get('phishingWhitelist', function (value) {
+    if (!value) {
+      value = []
+    }
 
-		var searchParams = new URLSearchParams(window.location.search.replace("?", ""));
+    var searchParams = new URLSearchParams(window.location.search.replace('?', ''))
 
-		var url = decodeURIComponent(searchParams.get("url"));
+    var url = decodeURIComponent(searchParams.get('url'))
 
-		if (!url) {
-			throw new Error("URL cannot be undefined");
-		}
+    if (!url) {
+      throw new Error('URL cannot be undefined')
+    }
 
-		var domain = new URL(url).hostname;
+    var domain = new URL(url).hostname
 
-		value.push(domain);
+    value.push(domain)
 
-		settings.set("phishingWhitelist", value);
+    settings.set('phishingWhitelist', value)
 
-		//redirect back to the original page
+        // redirect back to the original page
 
-		window.location = url;
-	});
-});
+    window.location = url
+  })
+})
 
-continueButton.addEventListener("click", function () {
-	window.location = "https://duckduckgo.com";
-});
+continueButton.addEventListener('click', function () {
+  window.location = 'https://duckduckgo.com'
+})
