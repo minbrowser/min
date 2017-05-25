aded172ed42d93bfdc174ebfb163a9cabca91ff0
code-hunger <code-hunger@users.noreply.github.com>
2017-05-26 01:57:04 +0300

    Added close buttons for tabs in taskOverlay (#352)
    * Close button for tabs added in taskOverlay
    
    * Silence npm install warning "missing repo field"
    
    * Better close button color based on page theme
    
    * Extract element insertion from function
    
    The goal is to simplify the function and avoid fetching the key-map-list
    elemnt everytime.
    Better name for the function used, too.
    
    * Split createTaskContainer into smaller parts
    
    * Extract tabContainer creation, too
    
    * Move overlay variable to the smallest scope needed
    
    Remove taskOverlay from some functions' parameters as it's a global
    variable
    
    * Fix preventDefault -> stopImmediatePropagation
    
    See https://github.com/minbrowser/min/pull/352/files/a2e92cebfaaf6a167665cec517fbe9010b324b90#r115128191
    
    * Separate creation and building of task tab element
    
    Still createTaskOverlayTabElement() needs a better name.
    
    * move taskContainer and closeButton creation to obj
    
    Moved these two functions to the TaskOverlayBuilder namespace
    Now that all creation-connected functions (except one) are split into
    pieces and grouped in a namespace it should be very easy to extract
    overlay building from using it into separate files. This greatly reduces
    complexity and improves readability.
    It also allows much easier creation of extensions to the taskOverlay. A
    few examples are keyboard-oriented control of the taskOverlay,
    fuzzy-search of tabs and more control buttons for tabs.
    
    * Group tab- and task-related builders in namespaces
    
    No functional changes, only separate builder.create.tab___()-like
    and builder.create.task____()-like functions into different namespaces
    
    * Alias TaskOverlayBuilder.create to builder
    
    It was too long, made it easier to use.
    
    * Remove createTaskOverlayTabElement
    
    Copied its code inside builder.tab.element as PalmerAL suggested
    
    * Fix broken getSelectedTask function
    
    * Fix destroyTab crash when tab is in another task
    
    * Fix bug when destroying current/last tab in task
    
    * Make taskOverlay independent of its builder
    
    Needed so that TaskOverlayBuilder can be taken out in a separate file
    
    * Move TaskOverlayBuilder into its own file
    
    * Use closeTabFromOverlay to handle all cases
    
    * Fixed typo, missing parameter in closeFromOverlay
    
    * fix formatting issues

---
+++ js/taskOverlay/taskOverlayBuilder.js
@@ -0,0 +1,162 @@
+function addTaskFromOverlay () {
+  tasks.setSelected(tasks.add())
+  taskOverlay.hide()
+
+  rerenderTabstrip()
+  addTab()
+}
+
+function removeTaskFromOverlay (tabId, task) {
+  task.tabs.destroy(tabId)
+  destroyWebview(tabId)
+
+  // if there are no tabs left, remove the task
+  if (task.tabs.count() === 0) {
+    destroyTask(task.id)
+    if (tasks.get().length === 0) {
+      addTaskFromOverlay()
+    } else {
+      // re-render the overlay to remove the task element
+      getTaskContainer(task.id).remove()
+    }
+  }
+}
+
+var TaskOverlayBuilder = {
+  create: {
+    task: {
+      nameInputField: function (task, taskIndex) {
+        var input = document.createElement('input')
+        input.classList.add('task-name')
+
+        input.placeholder = 'Task ' + (taskIndex + 1)
+        input.value = task.name || 'Task ' + (taskIndex + 1)
+
+        input.addEventListener('keyup', function (e) {
+          if (e.keyCode === 13) {
+            this.blur()
+          }
+          tasks.update(task.id, {name: this.value})
+        })
+
+        input.addEventListener('focus', function () {
+          this.select()
+        })
+        return input
+      },
+
+      deleteButton: function (container, task) {
+        var deleteButton = document.createElement('i')
+        deleteButton.className = 'fa fa-trash-o'
+
+        deleteButton.addEventListener('click', function (e) {
+          destroyTask(task.id)
+          container.remove()
+
+          if (tasks.get().length === 0) { // create a new task
+            addTaskFromOverlay()
+          }
+        })
+        return deleteButton
+      },
+
+      actionContainer: function (taskContainer, task, taskIndex) {
+        var taskActionContainer = document.createElement('div')
+        taskActionContainer.className = 'task-action-container'
+
+        // add the input for the task name
+        var input = this.nameInputField(task, taskIndex)
+        taskActionContainer.appendChild(input)
+
+        // add the delete button
+        var deleteButton = this.deleteButton(taskContainer, task)
+        taskActionContainer.appendChild(deleteButton)
+
+        return taskActionContainer
+      },
+      container: function (task, taskIndex) {
+        var container = document.createElement('div')
+        container.className = 'task-container'
+        container.setAttribute('data-task', task.id)
+
+        var taskActionContainer = this.actionContainer(container, task, taskIndex)
+        container.appendChild(taskActionContainer)
+
+        var tabContainer = TaskOverlayBuilder.create.tab.container(task)
+        container.appendChild(tabContainer)
+
+        return container
+      }
+    },
+
+    tab: {
+      element: function (tabContainer, task, tab) {
+        var el = createSearchbarItem({
+          title: tab.title || 'New Tab',
+          secondaryText: urlParser.removeProtocol(tab.url),
+          classList: ['task-tab-item'],
+          delete: function () {
+            removeTaskFromOverlay(tab.id, task)
+          }
+        })
+
+        el.setAttribute('data-tab', tab.id)
+        el.setAttribute('data-task', task.id)
+
+        el.addEventListener('click', function (e) {
+          switchToTask(this.getAttribute('data-task'))
+          switchToTab(this.getAttribute('data-tab'))
+
+          taskOverlay.hide()
+        })
+
+        var closeTabButton = this.closeButton(el)
+        el.querySelector('.title').appendChild(closeTabButton)
+        return el
+      },
+
+      container: function (task) {
+        var tabContainer = document.createElement('div')
+        tabContainer.className = 'task-tabs-container'
+
+        if (task.tabs) {
+          for (var i = 0; i < task.tabs.length; i++) {
+            var el = this.element(tabContainer, task, task.tabs[i])
+            tabContainer.appendChild(el)
+          }
+        }
+
+        return tabContainer
+      },
+
+      closeButton: function (taskTabElement) {
+        var closeTabButton = document.createElement('button')
+        closeTabButton.innerHTML = '✕'
+        closeTabButton.className = 'closeTab'
+
+        closeTabButton.addEventListener('click', function (e) {
+          var tabId = taskTabElement.getAttribute('data-tab')
+          var taskId = taskTabElement.getAttribute('data-task')
+          var current_selected = getSelectedTask()
+
+          if (tabId !== current_selected.tabs.getSelected()) {
+            removeTaskFromOverlay(tabId, tasks.get(taskId))
+            taskTabElement.parentNode.removeChild(taskTabElement)
+          }
+
+          // do not close taskOverlay
+          // (the close button is part of the tab-element, so a click on it
+          // would otherwise trigger opening this tab, and it was just closed)
+          e.stopImmediatePropagation()
+        })
+
+        return closeTabButton
+      }
+    }
+
+  }
+  // extend with other helper functions?
+}
+
+window.task_container_build_func = TaskOverlayBuilder.create.task.container.bind(TaskOverlayBuilder.create.task)
+
