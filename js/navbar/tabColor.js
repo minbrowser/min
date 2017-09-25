90509aff1550ddba58600cfb9079a66b577951ec
SpecialBomb <hawkingfan2000@gmail.com>
2017-09-25 11:31:34 -0400

    made tab dividers color reactive in order to look more subtle

--- js/navbar/tabColor.js
+++ js/navbar/tabColor.js
@@ -224,9 +224,56 @@ function updateColorPalette () {
   }
 }
 
+// Below function from http://jsfiddle.net/Mottie/xcqpF/1/light/
+
+function rgb2hex(rgb){
+	
+ rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ 
+ return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
+
+// Below function from https://css-tricks.com/snippets/javascript/lighten-darken-color/
+
+function lightenDarkenHexColor(col, amt) {
+  
+    var usePound = false;
+  
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+ 
+    var num = parseInt(col,16);
+ 
+    var r = (num >> 16) + amt;
+ 
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+ 
+    var b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+    var g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + String("000000" + (g | (b << 8) | (r << 16)).toString(16)).slice(-6);
+  
+}
+
 function setColor (bg, fg) {
   var background = document.getElementsByClassName('theme-background-color')
   var textcolor = document.getElementsByClassName('theme-text-color')
+  var tabs = document.getElementsByClassName('tab-item')
+  
+  console.log(bg)
 
   for (var i = 0; i < background.length; i++) {
     background[i].style.backgroundColor = bg
@@ -238,8 +285,14 @@ function setColor (bg, fg) {
 
   if (fg === 'white') {
     document.body.classList.add('dark-theme')
+    for (var i = 0; i < tabs.length; i++) {
+		tabs[i].style.borderRightColor = lightenDarkenHexColor(rgb2hex(bg), 20)
+	}
   } else {
     document.body.classList.remove('dark-theme')
+    for (var i = 0; i < tabs.length; i++) {
+		tabs[i].style.borderRightColor = lightenDarkenHexColor(rgb2hex(bg), -20)
+	}
   }
 }
 
