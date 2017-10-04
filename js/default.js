1d32f86f25cd35d6a0d6cd16e006bb8941b754b7
Ian Fare <apellius@gmail.com>
2017-10-04 19:04:55 -0400

    Fixed Issue #414: Added mouse movement threshold for clicking on tabs (#423)
    * Added mouse movement threshold for clicking on tabs
    
    * Simplify using movementX and movementY

--- js/default.js
+++ js/default.js
@@ -84,10 +84,12 @@ function empty (node) {
 window.addEventListener('load', function () {
   var isMouseDown = false
   var isDragging = false
+  var distance = 0
 
   document.body.addEventListener('mousedown', function () {
     isMouseDown = true
     isDragging = false
+    distance = 0
   })
 
   document.body.addEventListener('mouseup', function () {
@@ -97,15 +99,16 @@ window.addEventListener('load', function () {
   var dragHandles = document.getElementsByClassName('windowDragHandle')
 
   for (var i = 0; i < dragHandles.length; i++) {
-    dragHandles[i].addEventListener('mousemove', function () {
+    dragHandles[i].addEventListener('mousemove', function (e) {
       if (isMouseDown) {
         isDragging = true
+        distance += Math.abs(e.movementX) + Math.abs(e.movementY)
       }
     })
   }
 
-  document.body.addEventListener('click', function (e) {
-    if (isDragging) {
+  document.body.addEventListener('click', function (e) {    
+    if (isDragging && distance >= 10.0) {
       e.stopImmediatePropagation()
       isDragging = false
     }
