6320817fdc0562ccfc7e42214f84c7f581f06eea
PalmerAL <PalmerAL@users.noreply.github.com>
2017-11-04 20:37:14 -0500

    workaround for #378

--- js/webview/default.js
+++ js/webview/default.js
@@ -3,3 +3,11 @@
 var electron = require('electron')
 var ipc = electron.ipcRenderer
 var webFrame
+
+/* define window.chrome
+   this is necessary because some websites (such as the Google Drive file viewer, see issue #378) check for a
+   Chrome user agent, and then do things like if(chrome.<module>) {}
+   so we need to define an empty chrome object to prevent errors
+   */
+
+window.chrome = {}
