5476bfb0acdf441a9cefee857831852e429e1333
PalmerAL <PalmerAL@users.noreply.github.com>
2018-01-02 14:36:08 -0600

    reorganize settings js

--- pages/settings/settings.js
+++ pages/settings/settings.js
@@ -11,6 +11,27 @@ function showRestartRequiredBanner () {
   banner.hidden = false
 }
 
+/* tracking checkbox */
+
+settings.get('filtering', function (value) {
+  if (value) {
+    trackerCheckbox.checked = value.trackers
+  }
+})
+
+trackerCheckbox.addEventListener('change', function (e) {
+  settings.get('filtering', function (value) {
+    if (!value) {
+      value = {}
+    }
+    value.trackers = e.target.checked
+    settings.set('filtering', value)
+    banner.hidden = false
+  })
+})
+
+/* content type settings */
+
 var contentTypes = {
     // humanReadableName: contentType
   'scripts': 'script',
@@ -22,6 +43,123 @@ var contentTypeSettingNames = {
   'scripts': 'settingsBlockScriptsToggle',
   'images': 'settingsBlockImagesToggle'
 }
+
+for (var contentType in contentTypes) {
+  (function (contentType) {
+    settings.get('filtering', function (value) {
+      // create the settings section for blocking each content type
+
+      var section = document.createElement('div')
+      section.classList.add('setting-section')
+
+      var id = 'checkbox-block-' + contentTypes[contentType]
+
+      var checkbox = document.createElement('input')
+      checkbox.type = 'checkbox'
+      checkbox.id = id
+
+      if (value && value.contentTypes) {
+        checkbox.checked = value.contentTypes.indexOf(contentTypes[contentType]) != -1
+      }
+
+      var label = document.createElement('label')
+      label.setAttribute('for', id)
+      label.textContent = l(contentTypeSettingNames[contentType])
+
+      section.appendChild(checkbox)
+      section.appendChild(label)
+
+      container.appendChild(section)
+
+      checkbox.addEventListener('change', function (e) {
+        settings.get('filtering', function (value) {
+          if (!value) {
+            value = {}
+          }
+          if (!value.contentTypes) {
+            value.contentTypes = []
+          }
+
+          if (e.target.checked) { // add the item to the array
+            value.contentTypes.push(contentTypes[contentType])
+          } else { // remove the item from the array
+            var idx = value.contentTypes.indexOf(contentTypes[contentType])
+            value.contentTypes.splice(idx, 1)
+          }
+
+          settings.set('filtering', value)
+          banner.hidden = false
+        })
+      })
+    })
+  })(contentType)
+}
+
+/* dark mode setting */
+
+settings.get('darkMode', function (value) {
+  darkModeCheckbox.checked = value
+})
+
+darkModeCheckbox.addEventListener('change', function (e) {
+  settings.set('darkMode', this.checked)
+  showRestartRequiredBanner()
+})
+
+/* history button setting */
+
+settings.get('historyButton', function (value) {
+  if (value === true || value === undefined) {
+    historyButtonCheckbox.checked = true
+  } else {
+    historyButtonCheckbox.checked = false
+  }
+})
+
+historyButtonCheckbox.addEventListener('change', function (e) {
+  settings.set('historyButton', this.checked)
+  showRestartRequiredBanner()
+})
+
+/* swipe navigation settings */
+
+settings.get('swipeNavigationEnabled', function (value) {
+  if (value === true || value === undefined) {
+    swipeNavigationCheckbox.checked = true
+  } else {
+    swipeNavigationCheckbox.checked = false
+  }
+})
+
+swipeNavigationCheckbox.addEventListener('change', function (e) {
+  settings.set('swipeNavigationEnabled', this.checked)
+  showRestartRequiredBanner()
+})
+
+/* default search engine setting */
+
+var searchEngineDropdown = document.getElementById('default-search-engine')
+
+settings.onLoad(function () {
+  for (var searchEngine in searchEngines) {
+
+    var item = document.createElement('option')
+    item.textContent = searchEngines[searchEngine].name
+
+    if (searchEngines[searchEngine].name == currentSearchEngine.name) {
+      item.setAttribute('selected', 'true')
+    }
+
+    searchEngineDropdown.appendChild(item)
+  }
+})
+
+searchEngineDropdown.addEventListener('change', function (e) {
+  settings.set('searchEngine', this.value)
+  showRestartRequiredBanner()
+})
+
+/* key map settings */
 
 settings.get('keyMap', function (keyMapSettings) {
   var keyMap = userKeyMap(keyMapSettings)
@@ -81,142 +219,3 @@ function parseKeyInput (input) {
   parsed = parsed.filter(Boolean)
   return parsed.length > 1 ? parsed : parsed[0]
 }
-
-for (var contentType in contentTypes) {
-
-  (function (contentType) {
-
-    settings.get('filtering', function (value) {
-
-            // create the settings section for blocking each content type
-
-      var section = document.createElement('div')
-      section.classList.add('setting-section')
-
-      var id = 'checkbox-block-' + contentTypes[contentType]
-
-      var checkbox = document.createElement('input')
-      checkbox.type = 'checkbox'
-      checkbox.id = id
-
-      if (value && value.contentTypes) {
-        checkbox.checked = value.contentTypes.indexOf(contentTypes[contentType]) != -1
-      }
-
-      var label = document.createElement('label')
-      label.setAttribute('for', id)
-      label.textContent = l(contentTypeSettingNames[contentType])
-
-      section.appendChild(checkbox)
-      section.appendChild(label)
-
-      container.appendChild(section)
-
-      checkbox.addEventListener('change', function (e) {
-        settings.get('filtering', function (value) {
-          if (!value) {
-            value = {}
-          }
-          if (!value.contentTypes) {
-            value.contentTypes = []
-          }
-
-          if (e.target.checked) { // add the item to the array
-            value.contentTypes.push(contentTypes[contentType])
-          } else { // remove the item from the array
-            var idx = value.contentTypes.indexOf(contentTypes[contentType])
-            value.contentTypes.splice(idx, 1)
-          }
-
-          settings.set('filtering', value)
-          banner.hidden = false
-        })
-      })
-
-    })
-
-  })(contentType)
-
-}
-
-settings.get('filtering', function (value) {
-  if (value) {
-    trackerCheckbox.checked = value.trackers
-  }
-})
-
-trackerCheckbox.addEventListener('change', function (e) {
-  settings.get('filtering', function (value) {
-    if (!value) {
-      value = {}
-    }
-    value.trackers = e.target.checked
-    settings.set('filtering', value)
-    banner.hidden = false
-  })
-})
-
-// Dark Mode Settings
-settings.get('darkMode', function (value) {
-  darkModeCheckbox.checked = value
-})
-
-darkModeCheckbox.addEventListener('change', function (e) {
-  settings.set('darkMode', this.checked)
-  showRestartRequiredBanner()
-})
-
-// History buttons settings
-
-settings.get('historyButton', function (value) {
-  if (value === true || value === undefined) {
-    historyButtonCheckbox.checked = true
-  } else {
-    historyButtonCheckbox.checked = false
-  }
-})
-
-historyButtonCheckbox.addEventListener('change', function (e) {
-  settings.set('historyButton', this.checked)
-  showRestartRequiredBanner()
-})
-
-// Swipe navigation settings
-
-settings.get('swipeNavigationEnabled', function (value) {
-  if (value === true || value === undefined) {
-    swipeNavigationCheckbox.checked = true
-  } else {
-    swipeNavigationCheckbox.checked = false
-  }
-})
-
-swipeNavigationCheckbox.addEventListener('change', function (e) {
-  settings.set('swipeNavigationEnabled', this.checked)
-  showRestartRequiredBanner()
-})
-
-/* default search engine setting */
-
-var searchEngineDropdown = document.getElementById('default-search-engine')
-
-settings.onLoad(function () {
-
-  for (var searchEngine in searchEngines) {
-
-    var item = document.createElement('option')
-    item.textContent = searchEngines[searchEngine].name
-
-    if (searchEngines[searchEngine].name == currentSearchEngine.name) {
-      item.setAttribute('selected', 'true')
-    }
-
-    searchEngineDropdown.appendChild(item)
-  }
-
-})
-
-searchEngineDropdown.addEventListener('change', function (e) {
-  settings.set('searchEngine', this.value)
-  showRestartRequiredBanner()
-})
