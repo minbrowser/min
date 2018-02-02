87314b76c3c3ff26e27d1f5143b54f1aa205e29e
PalmerAL <PalmerAL@users.noreply.github.com>
2018-02-02 17:06:08 -0600

    reorganize the splash script

--- splash/splash.js
+++ splash/splash.js
@@ -2,50 +2,49 @@
 
 var failMessage = 'Min requires macOS or Linux'
 
-var availablePlatforms = ['MacIntel']
-
 var platforms = {
   'MacIntel': 'https://github.com/minbrowser/min/releases/download/v1.7.0/Min-v1.7.0-darwin-x64.zip',
   'Linux i686': 'https://github.com/minbrowser/min/releases/download/v1.7.0/Min_1.7.0_i386.deb',
   'x86_64': 'https://github.com/minbrowser/min/releases/download/v1.7.0/Min_1.7.0_amd64.deb'
 }
 
+function getDownloadLink() {
+  var downloadLink = null
+
+  for (var platform in platforms) {
+    if (navigator.platform.indexOf(platform) !== -1) {
+      downloadLink = platforms[platform]
+      break
+    }
+  }
+
+  // android often reports linux as the platform, but we don't have an android download
+
+  if (navigator.userAgent.indexOf('Android') !== -1) {
+    downloadLink = null
+  }
+
+  return downloadLink
+}
+
 var downloadButtons = document.getElementsByClassName('download-button')
 var subtexts = document.getElementsByClassName('button-subtext')
 
 // convert from a collection to an array, so the list doesn't change as we remove elements
-
 var subtextArray = []
 
 for (var i = 0; i < subtexts.length; i++) {
   subtextArray.push(subtexts[i])
 }
 
-var nav = navigator.platform
+var downloadLink = getDownloadLink();
 
-var platformMatched = false
-var downloadLink = null
-
-for (var platform in platforms) {
-  if (nav.indexOf(platform) !== -1) {
-    platformMatched = true
-    downloadLink = platforms[platform]
-    break
-  }
-}
-
-// android often reports linux as the platform
-
-if (navigator.userAgent.indexOf('Android') !== -1) {
-  platformMatched = false
-}
-
-if (platformMatched) {
+if (downloadLink) {
   for (var i = 0; i < downloadButtons.length; i++) {
     downloadButtons[i].parentElement.href = downloadLink
 
-        // show gatekeeper instruction popup
-    if (nav === 'MacIntel') {
+    // show gatekeeper instruction popup
+    if (navigator.platform === 'MacIntel') {
       downloadButtons[i].addEventListener('click', function () {
         setTimeout(openDownloadPopup, 500)
       }, false)
@@ -61,8 +60,7 @@ if (platformMatched) {
   }
 }
 
-if (platformMatched && nav != 'MacIntel') {
-
+if (downloadLink && navigator.platform !== 'MacIntel') {
   for (var i = 0; i < subtextArray.length; i++) {
     subtextArray[i].parentNode.removeChild(subtextArray[i])
   }
@@ -73,12 +71,12 @@ var dialog = document.getElementsByClassName('dialog')[0]
 
 var dialogCloseButtons = document.getElementsByClassName('dialog-close-button')
 
-function openDownloadPopup () {
+function openDownloadPopup() {
   backdrop.hidden = false
   dialog.hidden = false
 }
 
-function closeDownloadPopup () {
+function closeDownloadPopup() {
   backdrop.hidden = true
   dialog.hidden = true
 }
