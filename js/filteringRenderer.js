0250e702dd0d94c058879dd76a0f92978578c099
rob pike <r@golang.org>
2018-02-14 16:53:58 -0500

    fixed it

--- js/filteringRenderer.js
+++
@@ -1,9 +0,0 @@
-// gets the tracking settings and sends them to the main process
-
-settings.get('filtering', function (settings) {
-  ipc.send('setFilteringSettings', settings)
-})
-
-function registerFiltering (ses) {
-  ipc.send('registerFiltering', ses)
-}
--- js/navbar/addTabButton.js
+++
@@ -1,8 +0,0 @@
-var addTabButton = document.getElementById('add-tab-button')
-
-addTabButton.title = l('newTabAction')
-
-addTabButton.addEventListener('click', function (e) {
-  var newTab = tabs.add({}, tabs.getIndex(tabs.getSelected()) + 1)
-  addTab(newTab)
-})
