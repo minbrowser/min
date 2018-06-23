f41d679e9ecb8beaeb1ac0ee02587347b41c8674
PalmerAL <PalmerAL@users.noreply.github.com>
2018-06-23 12:14:44 -0500

    add build script for preload

--- scripts/watch.js
+++ scripts/watch.js
@@ -3,10 +3,17 @@ const path = require('path')
 
 const jsDir = path.resolve(__dirname, '../js')
 const mainDir = path.resolve(__dirname, '../main')
+const preloadDir = path.resolve(__dirname, '../js/webview')
 
 const buildBrowser = require('./buildBrowser.js')
+const buildPreload = require('./buildPreload.js')
 
-chokidar.watch([jsDir, mainDir]).on('change', function () {
+chokidar.watch([jsDir, mainDir], {ignored: preloadDir}).on('change', function () {
   console.log('rebuilding browser')
   buildBrowser()
+})
+
+chokidar.watch(preloadDir).on('change', function () {
+  console.log('rebuilding preload script')
+  buildPreload()
 })
