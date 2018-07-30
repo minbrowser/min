db8c7f74603559f468f3745df2e4644e325e2a12
PalmerAL <PalmerAL@users.noreply.github.com>
2018-07-29 21:51:29 -0500

    fix view focus issue

--- js/api-wrapper.js
+++ js/api-wrapper.js
@@ -54,7 +54,7 @@ function addTab (tabId, options) {
   }
 
   switchToTab(tabId, {
-    focusWebview: false
+    focusWebview: options.enterEditMode === false
   })
   if (options.enterEditMode !== false) {
     tabBar.enterEditMode(tabId)
