1198e92c583e360f0c6e115b5bf42a1f6fefabf6
Code Hunger <beinghavingbreackfast@gmail.com>
2018-09-11 23:58:15 +0300

    Extract big parts from webviewMenu.showMenu(). Reduce from 190 to 95 LOC

--- js/webviewMenu.js
+++ js/webviewMenu.js
@@ -2,8 +2,115 @@ var browserUI = require('api-wrapper.js')
 
 var Menu, MenuItem, clipboard // these are only loaded when the menu is shown
 
+const makeMenuItem = menuItem => new MenuItem(menuItem)
+const makeMenuItems = menuItems => menuItems.map(makeMenuItem)
+
+function makeLinkMenuItems (link, isPrivate) {
+  var linkActions = [
+    new MenuItem({
+      label: (link.length > 60) ? link.substring(0, 60) + '...' : link,
+      enabled: false
+    })
+  ]
+
+  if (!isPrivate) {
+    linkActions.push(new MenuItem({
+      label: l('openInNewTab'),
+      click: function () {
+        browserUI.addTab(tabs.add({ url: link }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
+      }
+    }))
+  }
+
+  linkActions.push(new MenuItem({
+    label: l('openInNewPrivateTab'),
+    click: function () {
+      browserUI.addTab(tabs.add({ url: link, private: true }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
+    }
+  }))
+
+  return linkActions
+}
+
+function makeImageMenuItems (image, isPrivate) {
+      /* images */
+      /* we don't show the image actions if there are already link actions, because it makes the menu too long and because the image actions typically aren't very useful if the image is a link */
+
+  var imageActions = [
+    new MenuItem({
+      label: (image.length > 60) ? image.substring(0, 60) + '...' : image,
+      enabled: false
+    })
+  ]
+
+  imageActions.push(new MenuItem({
+    label: l('viewImage'),
+    click: function () {
+      browserUI.navigate(tabs.getSelected(), image)
+    }
+  }))
+
+  if (!isPrivate) {
+    imageActions.push(new MenuItem({
+      label: l('openImageInNewTab'),
+      click: function () {
+        browserUI.addTab(tabs.add({ url: image }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
+      }
+    }))
+  }
+
+  imageActions.push(new MenuItem({
+    label: l('openImageInNewPrivateTab'),
+    click: function () {
+      browserUI.addTab(tabs.add({ url: image, private: true }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
+    }
+  }))
+
+  return imageActions
+}
+
+function makeSelectionMenuItems (selection, isPrivate, searchEngine) {
+  return [
+    new MenuItem({
+      label: l('searchWith').replace('%s', searchEngine.name),
+      click: function () {
+        var newTab = tabs.add({
+          url: searchEngine.searchURL.replace('%s', encodeURIComponent(selection)),
+          private: isPrivate
+        }, tabs.getIndex(tabs.getSelected()) + 1)
+        browserUI.addTab(newTab, {
+          enterEditMode: false
+        })
+
+        webviews.get(newTab).focus()
+      }
+    })
+  ]
+}
+
+function makeNavigationMenuItems () {
+  return [
+    new MenuItem({
+      label: l('goBack'),
+      click: function () {
+        try {
+          webviews.get(tabs.getSelected()).goBack()
+        } catch (e) {}
+      }
+    }),
+    new MenuItem({
+      label: l('goForward'),
+      click: function () {
+        try {
+          webviews.get(tabs.getSelected()).goForward()
+        } catch (e) {}
+      }
+    })
+  ]
+}
+
 var webviewMenu = {
-  showMenu: function (data) { // data comes from a context-menu event
+  showMenu: function (data, searchEngine) { // data comes from a context-menu event
     if (!Menu || !MenuItem || !clipboard) {
       Menu = remote.Menu
       MenuItem = remote.MenuItem
@@ -11,7 +118,7 @@ var webviewMenu = {
     }
 
     var menu = new Menu()
-    var currentTab = tabs.get(tabs.getSelected())
+    var currentTabIsPrivate = tabs.get(tabs.getSelected()).private
 
     var menuSections = []
 
@@ -22,65 +129,9 @@ var webviewMenu = {
     var image = data.srcURL
 
     if (link) {
-      var linkActions = [
-        new MenuItem({
-          label: (link.length > 60) ? link.substring(0, 60) + '...' : link,
-          enabled: false
-        })
-      ]
-
-      if (!currentTab.private) {
-        linkActions.push(new MenuItem({
-          label: l('openInNewTab'),
-          click: function () {
-            browserUI.addTab(tabs.add({ url: link }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
-          }
-        }))
-      }
-
-      linkActions.push(new MenuItem({
-        label: l('openInNewPrivateTab'),
-        click: function () {
-          browserUI.addTab(tabs.add({ url: link, private: true }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
-        }
-      }))
-
-      menuSections.push(linkActions)
+      menuSections.push(makeLinkMenuItems(link, currentTabIsPrivate))
     } else if (image) {
-      /* images */
-      /* we don't show the image actions if there are already link actions, because it makes the menu too long and because the image actions typically aren't very useful if the image is a link */
-
-      var imageActions = [
-        new MenuItem({
-          label: (image.length > 60) ? image.substring(0, 60) + '...' : image,
-          enabled: false
-        })
-      ]
-
-      imageActions.push(new MenuItem({
-        label: l('viewImage'),
-        click: function () {
-          browserUI.navigate(tabs.getSelected(), image)
-        }
-      }))
-
-      if (!currentTab.private) {
-        imageActions.push(new MenuItem({
-          label: l('openImageInNewTab'),
-          click: function () {
-            browserUI.addTab(tabs.add({ url: image }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
-          }
-        }))
-      }
-
-      imageActions.push(new MenuItem({
-        label: l('openImageInNewPrivateTab'),
-        click: function () {
-          browserUI.addTab(tabs.add({ url: image, private: true }, tabs.getIndex(tabs.getSelected()) + 1), { enterEditMode: false })
-        }
-      }))
-
-      menuSections.push(imageActions)
+      menuSections.push(makeImageMenuItems(image, currentTabIsPrivate))
 
       menuSections.push([
         new MenuItem({
@@ -97,23 +148,7 @@ var webviewMenu = {
     var selection = data.selectionText
 
     if (selection) {
-      var textActions = [
-        new MenuItem({
-          label: l('searchWith').replace('%s', currentSearchEngine.name),
-          click: function () {
-            var newTab = tabs.add({
-              url: currentSearchEngine.searchURL.replace('%s', encodeURIComponent(selection)),
-              private: currentTab.private
-            }, tabs.getIndex(tabs.getSelected()) + 1)
-            browserUI.addTab(newTab, {
-              enterEditMode: false
-            })
-
-            webviews.get(newTab).focus()
-          }
-        })
-      ]
-      menuSections.push(textActions)
+      menuSections.push(makeSelectionMenuItems(selection, currentTabIsPrivate, searchEngine))
     }
 
     var clipboardActions = []
@@ -149,26 +184,7 @@ var webviewMenu = {
       menuSections.push(clipboardActions)
     }
 
-    var navigationActions = [
-      new MenuItem({
-        label: l('goBack'),
-        click: function () {
-          try {
-            webviews.get(tabs.getSelected()).goBack()
-          } catch (e) {}
-        }
-      }),
-      new MenuItem({
-        label: l('goForward'),
-        click: function () {
-          try {
-            webviews.get(tabs.getSelected()).goForward()
-          } catch (e) {}
-        }
-      })
-    ]
-
-    menuSections.push(navigationActions)
+    menuSections.push(makeNavigationMenuItems())
 
     /* inspect element */
     menuSections.push([
