365c0ddf29bf746d261bd7e0a887dcc851adc2f9
PalmerAL <PalmerAL@users.noreply.github.com>
2018-10-28 20:01:26 -0500

    fix theme switching logic
    Previously, if the browser was started at a time when dark mode should
    be enabled, the timer was never created, so dark mode wouldn’t be
    disabled the next morning.

--- js/util/theme.js
+++ js/util/theme.js
@@ -3,24 +3,35 @@ function shouldEnableDarkMode () {
   return hours > 21 || hours < 6
 }
 
+function enableDarkMode () {
+  document.body.classList.add('dark-mode')
+  window.isDarkMode = true
+  window.dispatchEvent(new CustomEvent('themechange'))
+}
+
+function disableDarkMode () {
+  document.body.classList.remove('dark-mode')
+  window.isDarkMode = false
+  window.dispatchEvent(new CustomEvent('themechange'))
+}
+
 settings.get('darkMode', function (value) {
-  if (value === true || shouldEnableDarkMode()) {
-    document.body.classList.add('dark-mode')
-    window.isDarkMode = true
-    window.dispatchEvent(new CustomEvent('themechange'))
-  } else {
-    setInterval(function () {
-      if (shouldEnableDarkMode()) {
-        if (!window.isDarkMode) {
-          document.body.classList.add('dark-mode')
-          window.isDarkMode = true
-          window.dispatchEvent(new CustomEvent('themechange'))
-        }
-      } else if (window.isDarkMode) {
-        document.body.classList.remove('dark-mode')
-        window.isDarkMode = false
-        window.dispatchEvent(new CustomEvent('themechange'))
-      }
-    }, 10000)
+  if (value === true) {
+    enableDarkMode()
+    return
   }
+
+  if (shouldEnableDarkMode()) {
+    enableDarkMode()
+  }
+
+  setInterval(function () {
+    if (shouldEnableDarkMode()) {
+      if (!window.isDarkMode) {
+        enableDarkMode()
+      }
+    } else if (window.isDarkMode) {
+      disableDarkMode()
+    }
+  }, 10000)
 })
