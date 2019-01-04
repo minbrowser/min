9a99e2b9d539e79a8d891cf7d9f51431639336a7
PalmerAL <PalmerAL@users.noreply.github.com>
2019-01-04 17:16:22 -0600

    add third-party-blocking and exception settings

--- js/filteringRenderer.js
+++ js/filteringRenderer.js
@@ -1,7 +1,17 @@
 // gets the tracking settings and sends them to the main process
 
-settings.get('filtering', function (settings) {
-  ipc.send('setFilteringSettings', settings)
+settings.get('filtering', function (value) {
+  // migrate from old settings (<v1.9.0)
+  if (value && typeof value.trackers === 'boolean') {
+    if (value.trackers === true) {
+      value.blockingLevel = 2
+    } else if (value.trackers === false) {
+      value.blockingLevel = 0
+    }
+    delete value.trackers
+    settings.set('filtering', value)
+  }
+  ipc.send('setFilteringSettings', value)
 })
 
 function registerFiltering (ses) {
