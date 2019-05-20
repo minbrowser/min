8acbf833e4f9f116488fed42e28f703ca9a3348f
Toma <contact@thomas-favre.fr>
2019-05-20 11:32:30 +0200

    ♻️ refactor (context menu): reorder imports

--- js/contextMenu.js
+++ js/contextMenu.js
@@ -1,40 +1,37 @@
-window.electron = require('electron')
-window.remote = electron.remote
-window.Menu = remote.Menu;
+document.getElementById('tabs').addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (e.target.classList.contains('tab-input')){
+
+    remote = electron.remote, Menu = remote.Menu;
+
+    InputMenu = Menu.buildFromTemplate([{
+      label: l('undo'),
+      role: 'undo',
+    }, {
+      label: l('redo'),
+      role: 'redo',
+    }, {
+      type: 'separator',
+    }, {
+      label: l('cut'),
+      role: 'cut',
+    }, {
+      label: l('copy'),
+      role: 'copy',
+    }, {
+      label: l('paste'),
+      role: 'paste',
+    }, {
+      type: 'separator',
+    }, {
+      label: l('selectAll'),
+      role: 'selectall',
+    },
+    ]);
+      InputMenu.popup(remote.getCurrentWindow());
+  }
+});
 
 
-InputMenu = window.Menu.buildFromTemplate([{
-    label: l('undo'),
-    role: 'undo',
-  }, {
-    label: l('redo'),
-    role: 'redo',
-  }, {
-    type: 'separator',
-  }, {
-    label: l('cut'),
-    role: 'cut',
-  }, {
-    label: l('copy'),
-    role: 'copy',
-  }, {
-    label: l('paste'),
-    role: 'paste',
-  }, {
-    type: 'separator',
-  }, {
-    label: l('selectAll'),
-    role: 'selectall',
-  },
-  ]);
-  
-  document.body.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  
-    let node = e.target;
-
-    if (node.nodeName.match(/^(input|textarea)$/i) || node.isContentEditable) {
-        InputMenu.popup(remote.getCurrentWindow());
-    }
-  });
