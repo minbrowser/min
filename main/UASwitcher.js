e6049a07e2a4829ff40448af5d3ce38b67ccc61e
PalmerAL <PalmerAL@users.noreply.github.com>
2019-08-16 17:23:42 -0500

    fix setting user agent for service workers
    fixes #657

--- main/UASwitcher.js
+++ main/UASwitcher.js
@@ -1,16 +1,6 @@
 /* Use the same user agent as Chrome to improve site compatibility and increase fingerprinting resistance
 see https://github.com/minbrowser/min/issues/657 for more information */
 
-function setUserAgent (session) {
-  let defaultUserAgent = session.getUserAgent()
-  let newUserAgent = defaultUserAgent.replace(/Min\/\S+\s/, '').replace(/Electron\/\S+\s/, '')
-  session.setUserAgent(newUserAgent)
-}
-
-app.once('ready', function () {
-  setUserAgent(session.defaultSession)
-})
-
-app.on('session-created', function (session) {
-  setUserAgent(session)
-})
+let defaultUserAgent = app.userAgentFallback
+let newUserAgent = defaultUserAgent.replace(/Min\/\S+\s/, '').replace(/Electron\/\S+\s/, '')
+app.userAgentFallback = newUserAgent
