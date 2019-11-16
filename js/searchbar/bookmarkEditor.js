17572f6068e1a3a2df7e3ee9c78be92f5a17581a
PalmerAL <PalmerAL@users.noreply.github.com>
2019-11-15 22:29:22 -0600

    reorganize

---
+++ js/searchbar/bookmarkEditor.js
@@ -0,0 +1,130 @@
+var { db } = require('util/database.js')
+var places = require('places/places.js')
+
+const bookmarkEditor = {
+    currentInstance: null,
+    getTagElement: function (tag, selected, onClick) {
+        var el = document.createElement('button')
+        el.className = 'tag'
+        el.textContent = tag
+        if (selected) {
+          el.classList.add('selected')
+          el.setAttribute('aria-pressed', true)
+        } else {
+          el.classList.add('suggested')
+          el.setAttribute('aria-pressed', false)
+        }
+        el.addEventListener('click', function () {
+          onClick()
+          if (el.classList.contains('selected')) {
+            el.remove()
+          } else {
+            el.classList.add('selected')
+          }
+        })
+        return el
+      },
+    render: async function(url) {
+        bookmarkEditor.currentInstance = {};
+        bookmarkEditor.currentInstance.bookmark = await db.places.where('url').equals(url).first();
+  
+        var editor = document.createElement('div')
+        editor.className = 'bookmark-editor searchbar-item'
+      
+        //title input
+        var title = document.createElement("span")
+        title.className = "title wide";
+        title.textContent = bookmarkEditor.currentInstance.bookmark.title;
+        editor.appendChild(title);
+      
+        //URL
+        var URLSpan = document.createElement("div")
+        URLSpan.className = "bookmark-url";
+        URLSpan.textContent = bookmarkEditor.currentInstance.bookmark.url;
+        editor.appendChild(URLSpan);
+      
+        //save button
+        var saveButton = document.createElement('button')
+        saveButton.className = "fa fa-check action-button";
+        saveButton.tabIndex = -1
+        editor.appendChild(saveButton);
+        saveButton.addEventListener("click", function() {
+          editor.remove();
+          bookmarkEditor.currentInstance.onClose(bookmarkEditor.currentInstance.bookmark);
+         bookmarkEditor.currentInstance = null;
+        });
+      
+        //delete button
+        var delButton = document.createElement('button')
+        delButton.className = "fa fa-trash action-button bookmark-delete-button";
+        delButton.tabIndex = -1
+        editor.appendChild(delButton);
+        delButton.addEventListener("click", function() {
+          editor.remove();
+          bookmarkEditor.currentInstance.onClose(null);
+          bookmarkEditor.currentInstance = null;
+        });
+      
+        //tag area
+        var tagArea = document.createElement('div')
+        tagArea.className = 'tag-edit-area'
+        editor.appendChild(tagArea)
+      
+        var tags = {
+          selected: [],
+          suggested: []
+        }
+      
+        // show tags
+        bookmarkEditor.currentInstance.bookmark.tags.forEach(function (tag) {
+          tagArea.appendChild(bookmarkEditor.getTagElement(tag, true, function() {
+            places.toggleTag(bookmarkEditor.currentInstance.bookmark.url, tag)
+          }))
+        })
+        tags.selected = bookmarkEditor.currentInstance.bookmark.tags
+      
+        places.getSuggestedTags(bookmarkEditor.currentInstance.bookmark.url, function(suggestions) {
+          tags.suggested = tags.suggested.concat(suggestions)
+      
+          tags.suggested.filter((tag, idx) => {
+            return tags.suggested.indexOf(tag) === idx && !tags.selected.includes(tag)
+          }).slice(0, 3).forEach(function (tag, idx) {
+            tagArea.appendChild(bookmarkEditor.getTagElement(tag, false, function() {
+              places.toggleTag(bookmarkEditor.currentInstance.bookmark.url, tag)
+            }))
+          })
+          // add option for new tag
+          var newTagInput = document.createElement('input')
+          newTagInput.className = 'tag-input'
+          newTagInput.placeholder = 'Add tag...'
+          newTagInput.classList.add('mousetrap')
+          tagArea.appendChild(newTagInput)
+          newTagInput.addEventListener('change', function () {
+            if (!tags.selected.includes(this.value)) {
+              places.toggleTag(bookmarkEditor.currentInstance.bookmark.url, this.value)
+              tagArea.insertBefore(bookmarkEditor.getTagElement(this.value, true), tagArea.firstElementChild)
+            }
+            this.value = ''
+          })
+        })
+      
+        return editor;
+    },
+    show: function(url, replaceItem, onClose) {
+        if (bookmarkEditor.currentInstance) {
+            if (bookmarkEditor.currentInstance.editor.parentNode) {
+                bookmarkEditor.currentInstance.onClose(bookmarkEditor.currentInstance.bookmark);
+                bookmarkEditor.currentInstance.editor.remove();
+            }
+            bookmarkEditor.currentInstance = null;
+        }
+        bookmarkEditor.render(url).then(function(editor) {
+            replaceItem.hidden = true;
+            replaceItem.parentNode.insertBefore(editor, replaceItem);
+            bookmarkEditor.currentInstance.editor = editor;
+            bookmarkEditor.currentInstance.onClose = onClose;
+        })
+    },
+}
+
+module.exports = bookmarkEditor;
--- js/searchbar/bookmarkManager.js
+++ js/searchbar/bookmarkManager.js
@@ -3,159 +3,28 @@ var searchbarPlugins = require('searchbar/searchbarPlugins.js')
 var searchbarUtils = require('searchbar/searchbarUtils.js')
 var places = require('places/places.js')
 var urlParser = require('util/urlParser.js')
-var { db } = require('util/database.js')
 var formatRelativeDate = require('util/relativeDate.js')
 
-function getTagText(text) {
+var bookmarkEditor = require('searchbar/bookmarkEditor.js')
+
+function getTagText (text) {
   return text.split(/\s/g).filter(function (word) {
-    return word.startsWith("#") && word.length > 1
-  }).map(t => t.substring(1));
-}
-/*
-async function showTagSuggestions(container) {
-  var inputTags = getTagText(searchbar.getValue());
-  var tags = await db.places.orderBy("tags").keys()
-
-  var tagMap = {}
-  tags.forEach(function(tag) {
-    if (tagMap[tag]) {
-      tagMap[tag]++;
-    } else {
-      tagMap[tag] = 1;
-    }
-  })
-
-  var sortedTags = Object.keys(tagMap).sort((a, b) => {return tagMap[a] > tagMap[b]})
-
-  sortedTags.slice(0, 8).forEach(function(tag) {
-    var el = getTagElement(tag, false, function() {
-      alert(tag);
-    })
-    container.appendChild(el);
-  })
-}
-*/
-function getTagElement(tag, selected, onClick) {
-  var el = document.createElement('button')
-  el.className = 'tag'
-  el.textContent = tag
-  if (selected) {
-    el.classList.add('selected')
-    el.setAttribute('aria-pressed', true)
-  } else {
-    el.classList.add('suggested')
-    el.setAttribute('aria-pressed', false)
-  }
-  el.addEventListener('click', function () {
-    onClick();
-    if (el.classList.contains('selected')) {
-      el.remove()
-    } else {
-      el.classList.add('selected')
-    }
-  })
-  return el
+    return word.startsWith('#') && word.length > 1
+  }).map(t => t.substring(1))
 }
 
-async function getBookmarkEditor(url, closeFn) {
-  var bookmark = await db.places.where('url').equals(url).first();
-
-  var editor = document.createElement('div')
-  editor.className = 'bookmark-editor searchbar-item'
-
-  //title input
-  var title = document.createElement("span")
-  title.className = "title wide";
-  title.textContent = bookmark.title;
-  editor.appendChild(title);
-
-  //URL
-  var URLSpan = document.createElement("div")
-  URLSpan.className = "bookmark-url";
-  URLSpan.textContent = bookmark.url;
-  editor.appendChild(URLSpan);
-
-  //save button
-  var saveButton = document.createElement('button')
-  saveButton.className = "fa fa-check action-button";
-  saveButton.tabIndex = -1
-  editor.appendChild(saveButton);
-  saveButton.addEventListener("click", function(e) {
-    editor.remove();
-    closeFn(e, bookmark);
-  });
-
-  //delete button
-  var delButton = document.createElement('button')
-  delButton.className = "fa fa-trash action-button bookmark-delete-button";
-  delButton.tabIndex = -1
-  editor.appendChild(delButton);
-  delButton.addEventListener("click", function(e) {
-    editor.remove();
-    places.deleteHistory(url);
-    closeFn(e, null);
-  });
-
-  //tag area
-  var tagArea = document.createElement('div')
-  tagArea.className = 'tag-edit-area'
-  editor.appendChild(tagArea)
-
-  var tags = {
-    selected: [],
-    suggested: []
-  }
-
-  // show tags
-  bookmark.tags.forEach(function (tag) {
-    tagArea.appendChild(getTagElement(tag, true, function() {
-      places.toggleTag(bookmark.url, tag)
-    }))
-  })
-  tags.selected = bookmark.tags
-
-  places.getSuggestedTags(bookmark.url, function(suggestions) {
-    tags.suggested = tags.suggested.concat(suggestions)
-
-    tags.suggested.filter((tag, idx) => {
-      return tags.suggested.indexOf(tag) === idx && !tags.selected.includes(tag)
-    }).slice(0, 3).forEach(function (tag, idx) {
-      tagArea.appendChild(getTagElement(tag, false, function() {
-        places.toggleTag(bookmark.url, tag)
-      }))
-    })
-    // add option for new tag
-    var newTagInput = document.createElement('input')
-    newTagInput.className = 'tag-input'
-    newTagInput.placeholder = 'Add tag...'
-    newTagInput.classList.add('mousetrap')
-    tagArea.appendChild(newTagInput)
-    newTagInput.addEventListener('change', function () {
-      if (!tags.selected.includes(this.value)) {
-        places.toggleTag(bookmark.url, this.value)
-        tagArea.insertBefore(getTagElement(this.value, true), tagArea.firstElementChild)
-      }
-      this.value = ''
-    })
-  })
-
-  return editor;
-}
-
-function showBookmarkEditor(url, item) {
-  getBookmarkEditor(url, function onClose(e, newBookmark) {
+function showBookmarkEditor (url, item) {
+  bookmarkEditor.show(url, item, function (newBookmark) {
     if (newBookmark) {
-      item.parentNode.replaceChild(getBookmarkListItem(newBookmark), item);
+      item.parentNode.replaceChild(getBookmarkListItem(newBookmark), item)
     } else {
-      item.remove();
+      places.deleteHistory(url)
+      item.remove()
     }
-  }).then(function (editor) {
-    item.hidden = true;
-    item.parentNode.insertBefore(editor, item);
   })
 }
 
-function getBookmarkListItem(result, focus) {
+function getBookmarkListItem (result, focus) {
   var item = searchbarUtils.createItem({
     title: result.title,
     icon: 'fa-star',
@@ -177,7 +46,7 @@ function getBookmarkListItem(result, focus) {
       }
     }
   })
-  return item;
+  return item
 }
 
 bangsPlugin.registerCustomBang({
@@ -187,69 +56,78 @@ bangsPlugin.registerCustomBang({
   showSuggestions: function (text, input, event) {
     var container = searchbarPlugins.getContainer('bangs')
 
-    var originalText = text;
+    var originalText = text
 
     // filter out tags from the typed text
-    var searchedTags = getTagText(text);
+    var searchedTags = getTagText(text)
     searchedTags.forEach(function (word) {
-        text = text.replace("#" + word, '')
+      text = text.replace('#' + word, '')
     })
+    text = text.trim()
 
- //   showTagSuggestions(searchbarPlugins.topAnswerArea);
-
- var displayedURLset = []
+    var displayedURLset = []
     places.searchPlaces(text, function (results) {
       searchbarPlugins.reset('bangs')
 
-      var tagBar = document.createElement("div");
-      container.appendChild(tagBar);
+      var tagBar = document.createElement('div')
+      container.appendChild(tagBar)
 
-      places.autocompleteTags(searchedTags, function(suggestedTags) {
-        suggestedTags.forEach(function(suggestion) {
-          tagBar.appendChild(getTagElement(suggestion, false, function() {
-            tabBar.enterEditMode(tabs.getSelected(), "!bookmarks " + originalText + " #" + suggestion);
+      places.autocompleteTags(searchedTags, function (suggestedTags) {
+        searchedTags.forEach(function (tag) {
+          tagBar.appendChild(bookmarkEditor.getTagElement(tag, true, function () {
+            tabBar.enterEditMode(tabs.getSelected(), '!bookmarks ' + originalText.replace('#' + tag, ''))
           }))
         })
-      });
+        suggestedTags.forEach(function (suggestion) {
+          tagBar.appendChild(bookmarkEditor.getTagElement(suggestion, false, function () {
+            tabBar.enterEditMode(tabs.getSelected(), '!bookmarks ' + originalText + ' #' + suggestion)
+          }))
+        })
+      })
 
       var lastRelativeDate = '' // used to generate headings
 
-      results.sort(function (a, b) {
-        // order by last visit
-        return b.lastVisit - a.lastVisit
-      }).forEach(function (result, index) {
+      results
+      .filter(function (result) {
         for (var i = 0; i < searchedTags.length; i++) {
           if (!result.tags.filter(t => t.startsWith(searchedTags[i])).length) {
-            return
+            return false
           }
         }
-
-        displayedURLset.push(result.url);
+        return true
+      })
+      .sort(function (a, b) {
+        // order by last visit
+        return b.lastVisit - a.lastVisit
+      })
+      .slice(0, 100)
+      .forEach(function (result, index) {
+        displayedURLset.push(result.url)
 
         var thisRelativeDate = formatRelativeDate(result.lastVisit)
         if (thisRelativeDate !== lastRelativeDate) {
           searchbarPlugins.addHeading('bangs', { text: thisRelativeDate })
           lastRelativeDate = thisRelativeDate
         }
-        var item = getBookmarkListItem(result, index === 0 && text);
+        var item = getBookmarkListItem(result, index === 0 && text)
         container.appendChild(item)
       })
 
-      places.getSuggestedItemsForTags(searchedTags, function(suggestedResults) {
-        suggestedResults = suggestedResults.filter(res => !displayedURLset.includes(res.url));
+      places.getSuggestedItemsForTags(searchedTags, function (suggestedResults) {
+        suggestedResults = suggestedResults.filter(res => !displayedURLset.includes(res.url))
         if (suggestedResults.length === 0) {
-          return;
+          return
         }
-        searchbarPlugins.addHeading('bangs', { text: "Similar items" })
+        searchbarPlugins.addHeading('bangs', { text: 'Similar items' })
         suggestedResults.sort(function (a, b) {
           // order by last visit
           return b.lastVisit - a.lastVisit
         }).forEach(function (result, index) {
-          var item = getBookmarkListItem(result, false);
+          var item = getBookmarkListItem(result, false)
           container.appendChild(item)
         })
       })
-    }, { searchBookmarks: true, limit: (text ? 100 : Infinity) })
+    }, { searchBookmarks: true, limit: Infinity})
   },
   fn: function (text) {
     if (!text) {
