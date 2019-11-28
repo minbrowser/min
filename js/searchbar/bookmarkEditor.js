de17dd3bb233f61c4a5e47ecc9291e9c825dfe44
PalmerAL <PalmerAL@users.noreply.github.com>
2019-11-27 23:43:21 -0600

    don't hide editor buttons

--- js/searchbar/bookmarkEditor.js
+++ js/searchbar/bookmarkEditor.js
@@ -46,7 +46,7 @@ const bookmarkEditor = {
       
         //save button
         var saveButton = document.createElement('button')
-        saveButton.className = "fa fa-check action-button";
+        saveButton.className = "fa fa-check action-button always-visible";
         saveButton.tabIndex = -1
         editor.appendChild(saveButton);
         saveButton.addEventListener("click", function() {
@@ -57,7 +57,7 @@ const bookmarkEditor = {
       
         //delete button
         var delButton = document.createElement('button')
-        delButton.className = "fa fa-trash action-button bookmark-delete-button";
+        delButton.className = "fa fa-trash action-button always-visible bookmark-delete-button";
         delButton.tabIndex = -1
         editor.appendChild(delButton);
         delButton.addEventListener("click", function() {
