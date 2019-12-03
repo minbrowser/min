f52b2d1619aaa3094d7e65a49e62cc884b88817a
lee-yunseok <ironyunseok@protonmail.com>
2019-12-04 06:30:06 +0900

    armhf and korean

--- scripts/createPackage.js
+++ scripts/createPackage.js
@@ -36,6 +36,12 @@ var platformOptions = {
     platform: 'win32',
     icon: 'icons/icon256.ico'
   },
+  linux_armhf: {
+    name: 'min',
+    platform: 'linux',
+    arch: 'armv7l',
+    fpm: ['--architecture', 'armhf']
+  },
   linux: {
     name: 'min', // name must be lowercase to run correctly after installation
     platform: 'linux',
