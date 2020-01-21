7e3b91f0cd9f6331d0aa66c71a1c4b56ef840fec
thierry1970 <thierry@ordissimo.com>
2020-01-21 17:53:51 +0100

    Fix this issue : timche/gmail-desktop#174

--- main/UASwitcher.js
+++ main/UASwitcher.js
@@ -1,6 +1,6 @@
 /* Use the same user agent as Chrome to improve site compatibility and increase fingerprinting resistance
 see https://github.com/minbrowser/min/issues/657 for more information */
 
-let defaultUserAgent = app.userAgentFallback
+let defaultUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) old-airport-include/1.0.0 Chrome/79.0.3945.130 Electron/7.1.9 Safari/537.36'
 let newUserAgent = defaultUserAgent.replace(/Min\/\S+\s/, '').replace(/Electron\/\S+\s/, '')
 app.userAgentFallback = newUserAgent
