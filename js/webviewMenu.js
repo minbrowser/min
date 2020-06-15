minbrowser/min

commit 1c4ae7da3d4c5f90b2632ba54759a346b5711d14

remove remote module usage from context menus

js/webviewMenu.js
@@ -1,19 +1,12 @@
-var webviews = require('webviews.js')
-var browserUI = require('browserUI.js')
-var searchEngine = require('util/searchEngine.js')
+const webviews = require('webviews.js')
+const browserUI = require('browserUI.js')
+const searchEngine = require('util/searchEngine.js')
 
-var Menu, MenuItem, clipboard // these are only loaded when the menu is shown
+const remoteMenu = require('remoteMenuRenderer.js')
 
-var webviewMenu = {
+const webviewMenu = {
   menuData: null,
   showMenu: function (data, extraData) { // data comes from a context-menu event
-    if (!Menu || !MenuItem || !clipboard) {
-      Menu = remote.Menu
-      MenuItem = remote.MenuItem
-      clipboard = remote.clipboard
-    }
-
-    var menu = new Menu()
     var currentTab = tabs.get(tabs.getSelected())
 
     var menuSections = []
@@ -22,12 +15,12 @@ var webviewMenu = {
 
     if (extraData.hasVideo) {
       menuSections.push([
-        new MenuItem({
+        {
           label: l('pictureInPicture'),
           click: function () {
             webviews.callAsync(tabs.getSelected(), 'send', ['enterPictureInPicture', {x: data.x, y: data.y}])
           }
-        })
+        }
       ])
     }
 
@@ -44,27 +37,27 @@ var webviewMenu = {
 
     if (link) {
       var linkActions = [
-        new MenuItem({
+        {
           label: (link.length > 60) ? link.substring(0, 60) + '...' : link,
           enabled: false
-        })
+        }
       ]
 
       if (!currentTab.private) {
-        linkActions.push(new MenuItem({
+        linkActions.push({
           label: l('openInNewTab'),
           click: function () {
             browserUI.addTab(tabs.add({ url: link }), { enterEditMode: false })
           }
-        }))
+        })
       }
 
-      linkActions.push(new MenuItem({
+      linkActions.push({
         label: l('openInNewPrivateTab'),
         click: function () {
           browserUI.addTab(tabs.add({ url: link, private: true }), { enterEditMode: false })
         }
-      }))
+      })
 
       menuSections.push(linkActions)
     } else if (mediaURL && data.mediaType === 'image') {
@@ -72,44 +65,44 @@ var webviewMenu = {
       /* we don't show the image actions if there are already link actions, because it makes the menu too long and because the image actions typically aren't very useful if the image is a link */
 
       var imageActions = [
-        new MenuItem({
+        {
           label: (mediaURL.length > 60) ? mediaURL.substring(0, 60) + '...' : mediaURL,
           enabled: false
-        })
+        }
       ]
 
-      imageActions.push(new MenuItem({
+      imageActions.push({
         label: l('viewImage'),
         click: function () {
           webviews.update(tabs.getSelected(), mediaURL)
         }
-      }))
+      })
 
       if (!currentTab.private) {
-        imageActions.push(new MenuItem({
+        imageActions.push({
           label: l('openImageInNewTab'),
           click: function () {
             browserUI.addTab(tabs.add({ url: mediaURL }), { enterEditMode: false })
           }
-        }))
+        })
       }
 
-      imageActions.push(new MenuItem({
+      imageActions.push({
         label: l('openImageInNewPrivateTab'),
         click: function () {
           browserUI.addTab(tabs.add({ url: mediaURL, private: true }), { enterEditMode: false })
         }
-      }))
+      })
 
       menuSections.push(imageActions)
 
       menuSections.push([
-        new MenuItem({
+        {
           label: l('saveImageAs'),
           click: function () {
             remote.getCurrentWebContents().downloadURL(mediaURL)
           }
-        })
+        }
       ])
     }
 
@@ -119,7 +112,7 @@ var webviewMenu = {
 
     if (selection) {
       var textActions = [
-        new MenuItem({
+        {
           label: l('searchWith').replace('%s', searchEngine.getCurrent().name),
           click: function () {
             var newTab = tabs.add({
@@ -132,7 +125,7 @@ var webviewMenu = {
 
             webviews.get(newTab).focus()
           }
-        })
+        }
       ]
       menuSections.push(textActions)
     }
@@ -140,37 +133,37 @@ var webviewMenu = {
     var clipboardActions = []
 
     if (mediaURL && data.mediaType === 'image') {
-      clipboardActions.push(new MenuItem({
+      clipboardActions.push({
         label: l('copy'),
         click: function () {
           webviews.callAsync(tabs.getSelected(), 'copyImageAt', [data.x, data.y])
         }
-      }))
+      })
     } else if (selection) {
-      clipboardActions.push(new MenuItem({
+      clipboardActions.push({
         label: l('copy'),
         click: function () {
           clipboard.writeText(selection)
         }
-      }))
+      })
     }
 
     if (data.editFlags && data.editFlags.canPaste) {
-      clipboardActions.push(new MenuItem({
+      clipboardActions.push({
         label: l('paste'),
         click: function () {
           webviews.get(tabs.getSelected()).paste()
         }
-      }))
+      })
     }
 
     if (link || (mediaURL && !mediaURL.startsWith('blob:'))) {
-      clipboardActions.push(new MenuItem({
+      clipboardActions.push({
         label: l('copyLink'),
         click: function () {
           clipboard.writeText(link || mediaURL)
         }
-      }))
+      })
     }
 
     if (clipboardActions.length !== 0) {
@@ -178,50 +171,40 @@ var webviewMenu = {
     }
 
     var navigationActions = [
-      new MenuItem({
+      {
         label: l('goBack'),
         click: function () {
           try {
             webviews.goBackIgnoringRedirects(tabs.getSelected())
           } catch (e) {}
         }
-      }),
-      new MenuItem({
+      },
+      {
         label: l('goForward'),
         click: function () {
           try {
             webviews.get(tabs.getSelected()).goForward()
           } catch (e) {}
         }
-      })
+      }
     ]
 
     menuSections.push(navigationActions)
 
     /* inspect element */
     menuSections.push([
-      new MenuItem({
+      {
         label: l('inspectElement'),
         click: function () {
           webviews.get(tabs.getSelected()).inspectElement(data.x || 0, data.y || 0)
         }
-      })
+      }
     ])
-
-    menuSections.forEach(function (section) {
-      section.forEach(function (item) {
-        menu.append(item)
-      })
-      menu.append(new MenuItem({ type: 'separator' }))
-    })
 
     // Electron's default menu position is sometimes wrong on Windows with a touchscreen
     // https://github.com/minbrowser/min/issues/903
     var offset = webviews.getViewBounds()
-    menu.popup({
-      x: data.x + offset.x,
-      y: data.y + offset.y
-    })
+    remoteMenu.open(menuSections, data.x + offset.x, data.y + offset.y)
   },
   initialize: function () {
     webviews.bindEvent('context-menu', function (tabId, data) {

js/contextMenu.js
@@ -1,49 +1,49 @@
+const remoteMenu = require('remoteMenuRenderer.js')
+
 module.exports = {
   initialize: function () {
     document.addEventListener('contextmenu', (e) => {
       e.preventDefault()
       e.stopPropagation()
 
-      var Menu = remote.Menu
-
-      var InputMenu = Menu.buildFromTemplate([
-        {
-          label: l('undo'),
-          role: 'undo'
-        },
-        {
-          label: l('redo'),
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: l('cut'),
-          role: 'cut'
-        },
-        {
-          label: l('copy'),
-          role: 'copy'
-        },
-        {
-          label: l('paste'),
-          role: 'paste'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: l('selectAll'),
-          role: 'selectall'
-        }
-      ])
+      var inputMenu = [
+        [
+          {
+            label: l('undo'),
+            role: 'undo'
+          },
+          {
+            label: l('redo'),
+            role: 'redo'
+          }
+        ],
+        [
+          {
+            label: l('cut'),
+            role: 'cut'
+          },
+          {
+            label: l('copy'),
+            role: 'copy'
+          },
+          {
+            label: l('paste'),
+            role: 'paste'
+          }
+        ],
+        [
+          {
+            label: l('selectAll'),
+            role: 'selectall'
+          }
+        ]
+      ]
 
       let node = e.target
 
       while (node) {
         if (node.nodeName.match(/^(input|textarea)$/i) || node.isContentEditable) {
-          InputMenu.popup(remote.getCurrentWindow())
+          remoteMenu.open(inputMenu)
           break
         }
         node = node.parentNode

js/remoteMenuRenderer.js
@@ -1,0 +1,47 @@
+/*
+Passes a context menu template to the main process (where the menu is created)
+and listens for click events on it.
+*/
+
+var menuCallbacks = {}
+
+var nextMenuId = 0
+
+function open (menuTemplate, x, y) {
+  nextMenuId++
+  menuCallbacks[nextMenuId] = {}
+  var nextItemId = 0
+  function prepareToSend (menuPart) {
+    if (menuPart instanceof Array) {
+      return menuPart.map(item => prepareToSend(item))
+    } else {
+      if (typeof menuPart.click === 'function') {
+        menuCallbacks[nextMenuId][nextItemId] = menuPart.click
+        menuPart.click = nextItemId
+        nextItemId++
+      }
+      return menuPart
+    }
+  }
+
+  ipc.send('open-context-menu', {
+    id: nextMenuId,
+    template: prepareToSend(menuTemplate),
+    x,
+    y
+  })
+}
+
+ipc.on('context-menu-item-selected', function (e, data) {
+  menuCallbacks[data.menuId][data.itemId]()
+})
+
+ipc.on('context-menu-will-close', function (e, data) {
+  // delay close event until after selected event has been received
+  setTimeout(function () {
+    delete menuCallbacks[data.menuId]
+  }, 16)
+})
+
+module.exports = { open }
+

main/remoteMenu.js
@@ -1,0 +1,18 @@
+ipc.on('open-context-menu', function (e, data) {
+  var menu = new Menu()
+
+  data.template.forEach(function (section) {
+    section.forEach(function (item) {
+      var id = item.click
+      item.click = function () {
+        e.sender.send('context-menu-item-selected', { menuId: data.id, itemId: id })
+      }
+      menu.append(new MenuItem(item))
+    })
+    menu.append(new MenuItem({ type: 'separator' }))
+  })
+  menu.on('menu-will-close', function () {
+    e.sender.send('context-menu-will-close', {menuId: data.id})
+  })
+  menu.popup({ x: data.x, y: data.y })
+})

scripts/buildMain.js
@@ -15,10 +15,10 @@ const modules = [
   'main/UASwitcher.js',
   'main/permissionManager.js',
   'main/prompt.js',
+  'main/remoteMenu.js'
 ]
 
 function buildMain () {
-
   // build localization support first, since it is included in the bundle
   require('./buildLocalization.js')()
 

commit 76c203ccf2e117f9ded8209f1ad605592a0d8f40

fix copy actions in context menu
regression from 1c4ae7da3d4c5f90b2632ba54759a346b5711d14

js/webviewMenu.js
@@ -1,3 +1,5 @@
+const clipboard = remote.clipboard
+
 const webviews = require('webviews.js')
 const browserUI = require('browserUI.js')
 const searchEngine = require('util/searchEngine.js')