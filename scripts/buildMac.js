9576639bc3be1dbdec09abec4982af633c4347e4
PalmerAL <PalmerAL@users.noreply.github.com>
2020-11-05 23:07:11 -0600

    add apple silicon build

--- scripts/buildMac.js
+++ scripts/buildMac.js
@@ -6,17 +6,17 @@ const packageFile = require('./../package.json')
 const version = packageFile.version
 
 require('./createPackage.js')('darwin').then(function (appPaths) {
+  appPaths.forEach(function (packagePath) {
+    /* create zip file */
 
-  /* create zip files */
-  var packagePath = appPaths[0]
+    var output = fs.createWriteStream(packagePath.replace('Min-', 'Min-v' + version + '-') + '.zip')
+    var archive = archiver('zip', {
+      zlib: { level: 9 }
+    })
 
-  var output = fs.createWriteStream(packagePath.replace('Min-', 'Min-v' + version + '-') + '.zip')
-  var archive = archiver('zip', {
-    zlib: { level: 9 }
+    archive.directory(path.resolve(packagePath, 'Min.app'), 'Min.app')
+
+    archive.pipe(output)
+    archive.finalize()
   })
-
-  archive.directory(path.resolve(packagePath, 'Min.app'), 'Min.app')
-
-  archive.pipe(output)
-  archive.finalize()
 })
