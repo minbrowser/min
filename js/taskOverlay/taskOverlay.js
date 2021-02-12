minbrowser/min

commit 32d5dd8220842d78e38e5eaec02a3012565d6be5

convert taskOverlay to a module (#1448)

js/taskOverlay/taskOverlay.js
@@ -6,6 +6,7 @@ var tabEditor = require('navbar/tabEditor.js')
 var focusMode = require('focusMode.js')
 var modalMode = require('modalMode.js')
 var keyboardNavigationHelper = require('util/keyboardNavigationHelper.js')
+var dragula = require('dragula')
 
 const createTaskContainer = require('taskOverlay/taskOverlayBuilder.js')
 
@@ -15,25 +16,55 @@ var addTaskButton = document.getElementById('add-task')
 var addTaskLabel = addTaskButton.querySelector('span')
 var taskOverlayNavbar = document.getElementById('task-overlay-navbar')
 
-taskSwitcherButton.title = l('viewTasks')
-addTaskLabel.textContent = l('newTask')
+function addTaskFromMenu () {
+  /* new tasks can't be created in modal mode */
+  if (modalMode.enabled()) {
+    return
+  }
 
-taskSwitcherButton.addEventListener('click', function () {
-  taskOverlay.toggle()
-})
+  /* new tasks can't be created in focus mode or modal mode */
+  if (focusMode.enabled()) {
+    focusMode.warn()
+    return
+  }
 
-addTaskButton.addEventListener('click', function (e) {
-  browserUI.switchToTask(tasks.add())
-  taskOverlay.hide()
-})
+  browserUI.addTask()
+  taskOverlay.show()
+  setTimeout(function () {
+    taskOverlay.hide()
+    tabEditor.show(tabs.getSelected())
+  }, 600)
+}
 
-taskOverlayNavbar.addEventListener('click', function () {
-  taskOverlay.hide()
-})
+function getTaskContainer (id) {
+  return document.querySelector('.task-container[data-task="{id}"]'.replace('{id}', id))
+}
 
-var dragula = require('dragula')
+var draggingScrollInterval = null
 
-window.taskOverlay = {
+function onMouseMoveWhileDragging (e) {
+  clearInterval(draggingScrollInterval)
+  if (e.pageY < 100) {
+    draggingScrollInterval = setInterval(function () {
+      taskContainer.scrollBy(0, -5)
+    }, 16)
+  } else if (e.pageY > (window.innerHeight - 125)) {
+    draggingScrollInterval = setInterval(function () {
+      taskContainer.scrollBy(0, 5)
+    }, 16)
+  }
+}
+
+function startMouseDragRecording () {
+  window.addEventListener('mousemove', onMouseMoveWhileDragging)
+}
+
+function endMouseDragRecording () {
+  window.removeEventListener('mousemove', onMouseMoveWhileDragging)
+  clearInterval(draggingScrollInterval)
+}
+
+var taskOverlay = {
   overlayElement: document.getElementById('task-overlay'),
   isShown: false,
   tabDragula: dragula({
@@ -89,7 +120,27 @@ window.taskOverlay = {
 
     // show the task elements
     tasks.forEach(function (task, index) {
-      const el = createTaskContainer(task, index)
+      const el = createTaskContainer(task, index, {
+        tabSelect: function () {
+          browserUI.switchToTask(task.id)
+          browserUI.switchToTab(this.getAttribute('data-tab'))
+
+          taskOverlay.hide()
+        },
+        tabDelete: function (item) {
+          var tabId = item.getAttribute('data-tab')
+
+          browserUI.destroyTab(tabId)
+
+          // if there are no tabs left, remove the task
+          if (task.tabs.count() === 0) {
+            // remove the task element from the overlay
+            getTaskContainer(task.id).remove()
+            // close the task
+            browserUI.closeTask(task.id)
+          }
+        }
+      })
 
       taskContainer.appendChild(el)
       taskOverlay.tabDragula.containers.push(el.getElementsByClassName('task-tabs-container')[0])
@@ -154,192 +205,164 @@ window.taskOverlay = {
     } else {
       this.show()
     }
+  },
+
+  initialize: function () {
+    keyboardNavigationHelper.addToGroup('taskOverlay', taskOverlay.overlayElement)
+
+    // swipe down on the tabstrip to show the task overlay
+    document.getElementById('navbar').addEventListener('wheel', function (e) {
+      if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) {
+        // https://github.com/minbrowser/min/issues/698
+        return
+      }
+      if (e.deltaY < -30 && e.deltaX < 10) {
+        taskOverlay.show()
+        e.stopImmediatePropagation()
+      }
+    })
+
+    keybindings.defineShortcut('toggleTasks', function () {
+      if (taskOverlay.isShown) {
+        taskOverlay.hide()
+      } else {
+        taskOverlay.show()
+      }
+    })
+
+    keybindings.defineShortcut({ keys: 'esc' }, function (e) {
+      taskOverlay.hide()
+    })
+
+    keybindings.defineShortcut('enterEditMode', function (e) {
+      taskOverlay.hide()
+    })
+
+    keybindings.defineShortcut('addTask', addTaskFromMenu)
+    ipc.on('addTask', addTaskFromMenu) // for menu item
+
+    /* rearrange tabs when they are dropped */
+
+    taskOverlay.tabDragula.on('drag', function () {
+      taskOverlay.overlayElement.classList.add('is-dragging-tab')
+    })
+
+    taskOverlay.tabDragula.on('dragend', function () {
+      taskOverlay.overlayElement.classList.remove('is-dragging-tab')
+    })
+
+    taskOverlay.tabDragula.on('over', function (el, container, source) {
+      if (container === addTaskButton) {
+        addTaskButton.classList.add('drag-target')
+      }
+    })
+
+    taskOverlay.tabDragula.on('out', function (el, container, source) {
+      if (container === addTaskButton) {
+        addTaskButton.classList.remove('drag-target')
+      }
+    })
+
+    taskOverlay.tabDragula.on('drop', function (el, target, source, sibling) { // see https://github.com/bevacqua/dragula#drakeon-events
+      var tabId = el.getAttribute('data-tab')
+
+      var previousTask = tasks.get(source.getAttribute('data-task'))
+
+      // remove tab from old task
+      var oldTab = previousTask.tabs.splice(previousTask.tabs.getIndex(tabId), 1)[0]
+
+      // if the old task has no tabs left in it, destroy it
+
+      if (previousTask.tabs.count() === 0) {
+        browserUI.closeTask(previousTask.id)
+        getTaskContainer(previousTask.id).remove()
+      }
+
+      // if dropping on "add task" button, create a new task
+      if (target === addTaskButton) {
+        var newTask = tasks.get(tasks.add())
+        // remove from button, and re-create in overlay
+        el.remove()
+      } else {
+        // otherwise, find a source task to add this tab to
+        var newTask = tasks.get(target.getAttribute('data-task'))
+      }
+
+      if (sibling) {
+        var adjacentTadId = sibling.getAttribute('data-tab')
+      }
+
+      // find where in the new task the tab should be inserted
+      if (adjacentTadId) {
+        var newIdx = newTask.tabs.getIndex(adjacentTadId)
+      } else {
+        // tab was inserted at end
+        var newIdx = newTask.tabs.count()
+      }
+
+      // insert the tab at the correct spot
+      newTask.tabs.splice(newIdx, 0, oldTab)
+
+      tabBar.updateAll()
+      taskOverlay.show()
+    })
+
+    /* rearrange tasks when they are dropped */
+
+    taskOverlay.taskDragula.on('drop', function (el, target, source, sibling) {
+      var droppedTaskId = el.getAttribute('data-task')
+      if (sibling) {
+        var adjacentTaskId = sibling.getAttribute('data-task')
+      }
+
+      // remove the task from the tasks list
+      var droppedTask = tasks.splice(tasks.getIndex(droppedTaskId), 1)[0]
+
+      // find where it should be inserted
+      if (adjacentTaskId) {
+        var newIdx = tasks.getIndex(adjacentTaskId)
+      } else {
+        var newIdx = tasks.getLength()
+      }
+
+      // reinsert the task
+      tasks.splice(newIdx, 0, droppedTask)
+    })
+
+    /* auto-scroll the container when the item is dragged to the edge of the screen */
+
+    taskOverlay.tabDragula.on('drag', function () {
+      startMouseDragRecording()
+    })
+
+    taskOverlay.tabDragula.on('dragend', function () {
+      endMouseDragRecording()
+    })
+
+    taskOverlay.taskDragula.on('drag', function () {
+      startMouseDragRecording()
+    })
+
+    taskOverlay.taskDragula.on('dragend', function () {
+      endMouseDragRecording()
+    })
+
+    taskSwitcherButton.title = l('viewTasks')
+    addTaskLabel.textContent = l('newTask')
+
+    taskSwitcherButton.addEventListener('click', function () {
+      taskOverlay.toggle()
+    })
+
+    addTaskButton.addEventListener('click', function (e) {
+      browserUI.switchToTask(tasks.add())
+      taskOverlay.hide()
+    })
+
+    taskOverlayNavbar.addEventListener('click', function () {
+      taskOverlay.hide()
+    })
   }
 }
 
-keyboardNavigationHelper.addToGroup('taskOverlay', taskOverlay.overlayElement)
-
-// swipe down on the tabstrip to show the task overlay
-document.getElementById('navbar').addEventListener('wheel', function (e) {
-  if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) {
-    // https://github.com/minbrowser/min/issues/698
-    return
-  }
-  if (e.deltaY < -30 && e.deltaX < 10) {
-    taskOverlay.show()
-    e.stopImmediatePropagation()
-  }
-})
-
-keybindings.defineShortcut('toggleTasks', function () {
-  if (taskOverlay.isShown) {
-    taskOverlay.hide()
-  } else {
-    taskOverlay.show()
-  }
-})
-
-keybindings.defineShortcut({ keys: 'esc' }, function (e) {
-  taskOverlay.hide()
-})
-
-keybindings.defineShortcut('enterEditMode', function (e) {
-  taskOverlay.hide()
-})
-
-function addTaskFromMenu () {
-  /* new tasks can't be created in modal mode */
-  if (modalMode.enabled()) {
-    return
-  }
-
-  /* new tasks can't be created in focus mode or modal mode */
-  if (focusMode.enabled()) {
-    focusMode.warn()
-    return
-  }
-
-  browserUI.addTask()
-  taskOverlay.show()
-  setTimeout(function () {
-    taskOverlay.hide()
-    tabEditor.show(tabs.getSelected())
-  }, 600)
-}
-
-keybindings.defineShortcut('addTask', addTaskFromMenu)
-ipc.on('addTask', addTaskFromMenu) // for menu item
-
-function getTaskContainer (id) {
-  return document.querySelector('.task-container[data-task="{id}"]'.replace('{id}', id))
-}
-
-/* rearrange tabs when they are dropped */
-
-taskOverlay.tabDragula.on('drag', function () {
-  taskOverlay.overlayElement.classList.add('is-dragging-tab')
-})
-
-taskOverlay.tabDragula.on('dragend', function () {
-  taskOverlay.overlayElement.classList.remove('is-dragging-tab')
-})
-
-taskOverlay.tabDragula.on('over', function (el, container, source) {
-  if (container === addTaskButton) {
-    addTaskButton.classList.add('drag-target')
-  }
-})
-
-taskOverlay.tabDragula.on('out', function (el, container, source) {
-  if (container === addTaskButton) {
-    addTaskButton.classList.remove('drag-target')
-  }
-})
-
-taskOverlay.tabDragula.on('drop', function (el, target, source, sibling) { // see https://github.com/bevacqua/dragula#drakeon-events
-  var tabId = el.getAttribute('data-tab')
-
-  var previousTask = tasks.get(source.getAttribute('data-task'))
-
-  // remove tab from old task
-  var oldTab = previousTask.tabs.splice(previousTask.tabs.getIndex(tabId), 1)[0]
-
-  // if the old task has no tabs left in it, destroy it
-
-  if (previousTask.tabs.count() === 0) {
-    browserUI.closeTask(previousTask.id)
-    getTaskContainer(previousTask.id).remove()
-  }
-
-  // if dropping on "add task" button, create a new task
-  if (target === addTaskButton) {
-    var newTask = tasks.get(tasks.add())
-    // remove from button, and re-create in overlay
-    el.remove()
-  } else {
-    // otherwise, find a source task to add this tab to
-    var newTask = tasks.get(target.getAttribute('data-task'))
-  }
-
-  if (sibling) {
-    var adjacentTadId = sibling.getAttribute('data-tab')
-  }
-
-  // find where in the new task the tab should be inserted
-  if (adjacentTadId) {
-    var newIdx = newTask.tabs.getIndex(adjacentTadId)
-  } else {
-    // tab was inserted at end
-    var newIdx = newTask.tabs.count()
-  }
-
-  // insert the tab at the correct spot
-  newTask.tabs.splice(newIdx, 0, oldTab)
-
-  tabBar.updateAll()
-  taskOverlay.show()
-})
-
-/* rearrange tasks when they are dropped */
-
-taskOverlay.taskDragula.on('drop', function (el, target, source, sibling) {
-  var droppedTaskId = el.getAttribute('data-task')
-  if (sibling) {
-    var adjacentTaskId = sibling.getAttribute('data-task')
-  }
-
-  // remove the task from the tasks list
-  var droppedTask = tasks.splice(tasks.getIndex(droppedTaskId), 1)[0]
-
-  // find where it should be inserted
-  if (adjacentTaskId) {
-    var newIdx = tasks.getIndex(adjacentTaskId)
-  } else {
-    var newIdx = tasks.getLength()
-  }
-
-  // reinsert the task
-  tasks.splice(newIdx, 0, droppedTask)
-})
-
-/* auto-scroll the container when the item is dragged to the edge of the screen */
-
-var draggingScrollInterval = null
-
-function onMouseMoveWhileDragging (e) {
-  clearInterval(draggingScrollInterval)
-  if (e.pageY < 100) {
-    draggingScrollInterval = setInterval(function () {
-      taskContainer.scrollBy(0, -5)
-    }, 16)
-  } else if (e.pageY > (window.innerHeight - 125)) {
-    draggingScrollInterval = setInterval(function () {
-      taskContainer.scrollBy(0, 5)
-    }, 16)
-  }
-}
-
-function startMouseDragRecording () {
-  window.addEventListener('mousemove', onMouseMoveWhileDragging)
-}
-
-function endMouseDragRecording () {
-  window.removeEventListener('mousemove', onMouseMoveWhileDragging)
-  clearInterval(draggingScrollInterval)
-}
-
-taskOverlay.tabDragula.on('drag', function () {
-  startMouseDragRecording()
-})
-
-taskOverlay.tabDragula.on('dragend', function () {
-  endMouseDragRecording()
-})
-
-taskOverlay.taskDragula.on('drag', function () {
-  startMouseDragRecording()
-})
-
-taskOverlay.taskDragula.on('dragend', function () {
-  endMouseDragRecording()
-})
+module.exports = taskOverlay

js/browserUI.js
@@ -11,7 +11,6 @@ var searchbar = require('searchbar/searchbar.js')
 
 function addTask () {
   tasks.setSelected(tasks.add())
-  taskOverlay.hide()
 
   tabBar.updateAll()
   addTab()

js/default.js
@@ -150,6 +150,7 @@ require('passwordManager/passwordViewer.js').initialize()
 require('util/theme.js').initialize()
 require('userscripts.js').initialize()
 require('statistics.js').initialize()
+require('taskOverlay/taskOverlay.js').initialize()
 
 // default searchbar plugins
 

js/menuRenderer.js
@@ -9,6 +9,7 @@ var findinpage = require('findinpage.js')
 var PDFViewer = require('pdfViewer.js')
 var tabEditor = require('navbar/tabEditor.js')
 var readerView = require('readerView.js')
+var taskOverlay = require('taskOverlay/taskOverlay.js')
 
 module.exports = {
   initialize: function () {

js/navbar/menuButton.js
@@ -1,3 +1,5 @@
+var keybindings = require('keybindings.js')
+
 var menuButton = document.getElementById('menu-button')
 
 menuButton.addEventListener('click', function (e) {

js/searchbar/customBangs.js
@@ -12,6 +12,7 @@ var urlParser = require('util/urlParser.js')
 var { db } = require('util/database.js')
 var formatRelativeDate = require('util/relativeDate.js')
 var contentBlockingToggle = require('navbar/contentBlockingToggle.js')
+var taskOverlay = require('taskOverlay/taskOverlay.js')
 
 bangsPlugin.registerCustomBang({
   phrase: '!settings',

js/taskOverlay/taskOverlayBuilder.js
@@ -26,25 +26,6 @@ function getTaskRelativeDate (task) {
   }
 }
 
-function getTaskContainer (id) {
-  return document.querySelector('.task-container[data-task="{id}"]'.replace('{id}', id))
-}
-
-function removeTabFromOverlay (tabId, task) {
-  task.tabs.destroy(tabId)
-  webviews.destroy(tabId)
-
-  tabBar.updateAll()
-
-  // if there are no tabs left, remove the task
-  if (task.tabs.count() === 0) {
-    // remove the task element from the overlay
-    getTaskContainer(task.id).remove()
-    // close the task
-    browserUI.closeTask(task.id)
-  }
-}
-
 function toggleCollapsed (taskContainer, task) {
   tasks.get(task.id).collapsed = !tasks.isCollapsed(task.id)
   taskContainer.classList.toggle('collapsed')
@@ -219,7 +200,7 @@ var TaskOverlayBuilder = {
 
         return infoContainer
       },
-      container: function (task, taskIndex) {
+      container: function (task, taskIndex, events) {
         var container = document.createElement('div')
         container.className = 'task-container'
 
@@ -250,7 +231,7 @@ var TaskOverlayBuilder = {
         var deleteWarning = this.deleteWarning(container, task)
         container.appendChild(deleteWarning)
 
-        var tabContainer = TaskOverlayBuilder.create.tab.container(task)
+        var tabContainer = TaskOverlayBuilder.create.tab.container(task, events)
         container.appendChild(tabContainer)
 
         return container
@@ -258,12 +239,10 @@ var TaskOverlayBuilder = {
     },
 
     tab: {
-      element: function (tabContainer, task, tab) {
+      element: function (tabContainer, task, tab, events) {
         var data = {
           classList: ['task-tab-item'],
-          delete: function () {
-            removeTabFromOverlay(tab.id, task)
-          },
+          delete: events.tabDelete,
           showDeleteButton: true
         }
 
@@ -292,23 +271,18 @@ var TaskOverlayBuilder = {
 
         el.setAttribute('data-tab', tab.id)
 
-        el.addEventListener('click', function (e) {
-          browserUI.switchToTask(this.parentNode.getAttribute('data-task'))
-          browserUI.switchToTab(this.getAttribute('data-tab'))
-
-          taskOverlay.hide()
-        })
+        el.addEventListener('click', events.tabSelect)
         return el
       },
 
-      container: function (task) {
+      container: function (task, events) {
         var tabContainer = document.createElement('ul')
         tabContainer.className = 'task-tabs-container'
         tabContainer.setAttribute('data-task', task.id)
 
         if (task.tabs) {
           for (var i = 0; i < task.tabs.count(); i++) {
-            var el = this.element(tabContainer, task, task.tabs.getAtIndex(i))
+            var el = this.element(tabContainer, task, task.tabs.getAtIndex(i), events)
             tabContainer.appendChild(el)
           }
         }
@@ -320,6 +294,6 @@ var TaskOverlayBuilder = {
 // extend with other helper functions?
 }
 
-module.exports = function createTaskContainer (task, index) {
-  return TaskOverlayBuilder.create.task.container(task, index)
+module.exports = function createTaskContainer (task, index, events) {
+  return TaskOverlayBuilder.create.task.container(task, index, events)
 }

scripts/buildBrowser.js
@@ -16,7 +16,6 @@ const legacyModules = [
   'js/default.js',
   'js/windowControls.js',
   'js/searchbar/customBangs.js',
-  'js/taskOverlay/taskOverlay.js',
   'js/navbar/addTabButton.js',
   'js/navbar/menuButton.js',
   'js/sessionRestore.js'

commit 091d3e6714e14a2a6995ce86a4e17f2a2d0ce0e6

fix destroying tabs from other tasks
browserUI.destroyTab assumes the tab is in the current task, which
isn’t true here, so the tab needs to be destroyed manually instead

Fixes #1498, which is a regression from
32d5dd8220842d78e38e5eaec02a3012565d6be5

js/taskOverlay/taskOverlay.js
@@ -132,7 +132,10 @@ var taskOverlay = {
         tabDelete: function (item) {
           var tabId = item.getAttribute('data-tab')
 
-          browserUI.destroyTab(tabId)
+          tasks.get(task.id).tabs.destroy(tabId)
+          webviews.destroy(tabId)
+
+          tabBar.updateAll()
 
           // if there are no tabs left, remove the task
           if (task.tabs.count() === 0) {