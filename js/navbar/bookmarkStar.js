c853dde8ae08ca1611a9933a0174c0919abb01e3
PalmerAL <PalmerAL@users.noreply.github.com>
2021-06-14 22:32:10 -0500

    open bookmark editor instead of deleting bookmark when star clicked (#1652)

--- js/navbar/bookmarkStar.js
+++ js/navbar/bookmarkStar.js
@@ -23,24 +23,26 @@ const bookmarkStar = {
 
     searchbarPlugins.clearAll()
 
-    star.classList.toggle('carbon:star')
-    star.classList.toggle('carbon:star-filled')
+    places.updateItem(tabs.get(tabId).url, {
+      isBookmarked: true,
+      title: tabs.get(tabId).title // if this page is open in a private tab, the title may not be saved already, so it needs to be included here
+    }, function () {
+      star.classList.remove('carbon:star')
+      star.classList.add('carbon:star-filled')
+      star.setAttribute('aria-pressed', true)
 
-    places.toggleBookmarked(tabId, function (isBookmarked) {
-      if (isBookmarked) {
-        star.classList.remove('carbon:star')
-        star.classList.add('carbon:star-filled')
-        // since the update happens asynchronously, and star.update() could be called after onClick but before the update, it's possible for the classes to get out of sync with the actual bookmark state. Updating them here fixes tis.
-        star.setAttribute('aria-pressed', true)
-        var editorInsertionPoint = document.createElement('div')
-        searchbarPlugins.getContainer('simpleBookmarkTagInput').appendChild(editorInsertionPoint)
-        bookmarkEditor.show(tabs.get(tabs.getSelected()).url, editorInsertionPoint, null, { simplified: true, autoFocus: true })
-      } else {
-        star.classList.add('carbon:star')
-        star.classList.remove('carbon:star-filled')
-        star.setAttribute('aria-pressed', false)
-        searchbar.showResults('')
-      }
+      var editorInsertionPoint = document.createElement('div')
+      searchbarPlugins.getContainer('simpleBookmarkTagInput').appendChild(editorInsertionPoint)
+      bookmarkEditor.show(tabs.get(tabs.getSelected()).url, editorInsertionPoint, function (newBookmark) {
+        if (!newBookmark) {
+          // bookmark was deleted
+          star.classList.add('carbon:star')
+          star.classList.remove('carbon:star-filled')
+          star.setAttribute('aria-pressed', false)
+          searchbar.showResults('')
+          searchbar.associatedInput.focus()
+        }
+      }, { simplified: true, autoFocus: true })
     })
   },
   update: function (tabId, star) {
@@ -55,7 +57,7 @@ const bookmarkStar = {
 
     // check if the page is bookmarked or not, and update the star to match
 
-    db.places.where('url').equals(currentURL).first(function (item) {
+    places.getItem(currentURL, function (item) {
       if (item && item.isBookmarked) {
         star.classList.remove('carbon:star')
         star.classList.add('carbon:star-filled')
