d824b146c05b041e0ef8f808def2a25ad461d283
PalmerAL <PalmerAL@users.noreply.github.com>
2021-06-19 18:04:58 -0500

    don't save tab loading status to disk

--- js/tabState/tab.js
+++ js/tabState/tab.js
@@ -6,7 +6,7 @@ class TabList {
 
   //tab properties that shouldn't be saved to disk
 
-  static temporaryProperties = ['hasAudio', 'previewImage']
+  static temporaryProperties = ['hasAudio', 'previewImage', 'loaded']
 
   add (tab = {}, options = {}) {
     var tabId = String(tab.id || Math.round(Math.random() * 100000000000000000)) // you can pass an id that will be used, or a random one will be generated.
