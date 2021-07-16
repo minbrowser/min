77398c7249e937f37ecde7b8ff44e3cf0de1e9de
lubiedo <lisandro.ubiedo@gmail.com>
2021-07-16 11:39:49 -0600

    Copy result to clipboard
    Some other small changes.

--- js/searchbar/calculatorPlugin.js
+++ js/searchbar/calculatorPlugin.js
@@ -1,3 +1,8 @@
+/*
+  this plugin will provide the ability to perform simple calculations or to
+  convert to different types of units (ie. 12 lbs to kg = 5.44310844 kg)
+*/
+
 const { clipboard } = require('electron')
 const searchbarPlugins = require('searchbar/searchbarPlugins.js')
 const math = require('mathjs')
@@ -5,33 +10,38 @@ const math = require('mathjs')
 function doMath (text, input, event) {
   searchbarPlugins.reset('calculatorPlugin')
 
+  var result
   try {
-    const result = math.evaluate(text)
-    if (result !== undefined) {
-      searchbarPlugins.addResult('calculatorPlugin', {
-        title: '∑',
-        secondaryText: ` = ${result.toString()}`,
-      })
-    }
+    result = math.evaluate(text)
+  } catch (e) { return }
 
-    const container = searchbarPlugins.getContainer('calculatorPlugin')
+  searchbarPlugins.addResult('calculatorPlugin', {
+    title: '∑',
+    secondaryText: ` = ${result.toString()}`
+  })
 
-    if (container.childNodes.length === 1) {
-      const item = container.childNodes[0]
-      item.setAttribute('title', 'Click to copy')
-      item.addEventListener('click', (e) => {
-        const text = e.target.innerText
-        clipboard.writeText(text.slice(text.indexOf('=')+2))
-      })
-    }
-  } catch (e) { }
+  const container = searchbarPlugins.getContainer('calculatorPlugin')
+
+  if (container.childNodes.length === 1) {
+    const item = container.childNodes[0]
+    item.setAttribute('title', l('clickToCopy'))
+    item.addEventListener('click', (e) => {
+      const text = e.target.innerText
+      clipboard.writeText(text.slice(text.indexOf('=') + 2))
+    })
+  }
 }
 
 function initialize () {
   searchbarPlugins.register('calculatorPlugin', {
     index: 1,
     trigger: function (text) {
-      return text.length > 2 && /^[0-9.\-+*/()%^]+$/i.test(text)
+      if (text.length <= 2) {
+        return false
+      }
+
+      try { math.parse(text) } catch (e) { return false }
+      return true
     },
     showResults: debounce(doMath, 200)
   })
