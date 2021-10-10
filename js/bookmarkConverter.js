cf4f3a87a92ea4505f6ce18263594f926f70ab12
PalmerAL <PalmerAL@users.noreply.github.com>
2021-10-10 18:53:51 -0500

    add minimum backup size

--- js/bookmarkConverter.js
+++ js/bookmarkConverter.js
@@ -88,16 +88,22 @@ const bookmarkConverter = {
   initialize: function () {
     // how often to create a new backup file
     const interval = (3 * 24 * 60 * 60 * 1000)
+    // min size in bytes for a backup
+    // This is necessary because after the database is destroyed, the browser will launch with no bookmarks
+    // and the bookmarks backup shouldn't be overwritten in that case
+    const minSize = 512
 
     const checkAndExport = function () {
       if (!settings.get('lastBookmarksBackup') || (Date.now() - settings.get('lastBookmarksBackup')) > interval) {
         bookmarkConverter.exportAll().then(function (res) {
-          fs.writeFile(path.join(window.globalArgs['user-data-path'], 'bookmarksBackup.html'), res, { encoding: 'utf-8' }, function (err) {
-            if (err) {
-              console.warn(err)
-            }
-          })
-          settings.set('lastBookmarksBackup', Date.now())
+          if (res.length > minSize) {
+            fs.writeFile(path.join(window.globalArgs['user-data-path'], 'bookmarksBackup.html'), res, { encoding: 'utf-8' }, function (err) {
+              if (err) {
+                console.warn(err)
+              }
+            })
+            settings.set('lastBookmarksBackup', Date.now())
+          }
         })
           .catch(e => console.warn('error generating bookmarks backup', e))
       }
