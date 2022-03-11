227caa70d21a380e1cf0fe9d9d484485e76868ae
PalmerAL <PalmerAL@users.noreply.github.com>
2022-03-10 20:45:53 -0600

    improve touchbar icon sizing; add forward button

--- main/touchbar.js
+++ main/touchbar.js
@@ -7,28 +7,44 @@ function buildTouchBar () {
     return null
   }
 
+  function getTouchBarIcon (name) {
+    // the icons created by nativeImage are too big by default, shrink them to the correct size for the touchbar
+    var image = nativeImage.createFromNamedImage(name, [-1, 0, 1])
+    var size = image.getSize()
+    return image.resize({
+      width: Math.round(size.width * 0.65),
+      height: Math.round(size.height * 0.65)
+    })
+  }
   return new TouchBar({
     items: [
       new TouchBarButton({
         accessibilityLabel: l('goBack'),
-        icon: nativeImage.createFromNamedImage('NSImageNameTouchBarGoBackTemplate', [-1, 0, 1]),
+        icon: getTouchBarIcon('NSImageNameTouchBarGoBackTemplate'),
         click: function () {
           sendIPCToWindow(mainWindow, 'goBack')
         }
       }),
+      new TouchBarButton({
+        accessibilityLabel: l('goForward'),
+        icon: getTouchBarIcon('NSImageNameTouchBarGoForwardTemplate'),
+        click: function () {
+          sendIPCToWindow(mainWindow, 'goForward')
+        }
+      }),
       new TouchBarSpacer({ size: 'flexible' }),
       new TouchBarButton({
-        icon: nativeImage.createFromNamedImage('NSImageNameTouchBarSearchTemplate', [-1, 0, 1]),
+        icon: getTouchBarIcon('NSImageNameTouchBarSearchTemplate'),
         iconPosition: 'left',
         // TODO this is really hacky, find a better way to set the size
-        label: '    ' + l('searchbarPlaceholder') + '                                ',
+        label: '    ' + l('searchbarPlaceholder') + '                     ',
         click: function () {
           sendIPCToWindow(mainWindow, 'openEditor')
         }
       }),
       new TouchBarSpacer({ size: 'flexible' }),
       new TouchBarButton({
-        icon: nativeImage.createFromNamedImage('NSImageNameTouchBarAdd', [-1, 0, 1]),
+        icon: getTouchBarIcon('NSImageNameTouchBarAdd'),
         accessibilityLabel: l('newTabAction'),
         click: function () {
           sendIPCToWindow(mainWindow, 'addTab')
@@ -36,7 +52,7 @@ function buildTouchBar () {
       }),
       new TouchBarButton({
         accessibilityLabel: l('viewTasks'),
-        icon: nativeImage.createFromNamedImage('NSImageNameTouchBarListViewTemplate', [-1, 0, 1]),
+        icon: getTouchBarIcon('NSImageNameTouchBarListViewTemplate'),
         click: function () {
           sendIPCToWindow(mainWindow, 'toggleTaskOverlay')
         }
