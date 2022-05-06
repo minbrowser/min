e9661585da991d6b93ad45d0d58e6e7974a885d0
Samuel Hutchinson <34013162+flightmansam@users.noreply.github.com>
2022-05-06 23:50:43 +1000

    Added a fuzzy search + refactor

--- js/searchbar/customBangs.js
+++ js/searchbar/customBangs.js
@@ -87,6 +87,49 @@ function getTaskByNameOrNumber(text) {
   )
 }
 
+// return an array of dicts, sorted by last task activity
+// if a search string is present (and not a number) filter the results with a basic fuzzy search
+function searchAndSortTasks(text) {
+  var sortLastActivity = tasks.map(t => Object.assign({}, { task: t }, { lastActivity: tasks.getLastActivity(t.id) }))
+
+  sortLastActivity = sortLastActivity.sort(function (a, b) {
+    return b.lastActivity - a.lastActivity
+  })
+
+  var isSingleNumber = /^\d+$/.test(text)
+
+  if (text !== '' ? !isSingleNumber : !isSingleNumber) { //lXOR
+    // fuzzy search
+    var matches = []
+    var searchText = text.toLowerCase()
+
+    sortLastActivity.forEach(function (t) {
+
+      var task = t.task
+      var taskName = (task.name ? task.name : l('defaultTaskName').replace('%n', tasks.getIndex(task.id) + 1)).toLowerCase()
+      var exactMatch = taskName.indexOf(searchText) !== -1
+      var fuzzyTitleScore = taskName.score(searchText, 0.5)
+
+      if (exactMatch || fuzzyTitleScore > 0.4) {
+        matches.push({
+          task: t,
+          score: fuzzyTitleScore + exactMatch
+        })
+      }
+    })
+
+    matches = matches.sort(function (a, b) {
+      return b.score - a.score
+    })
+
+    sortLastActivity = matches.map(t => t.task)
+
+    selected = sortLastActivity[0]
+
+  }
+
+  return sortLastActivity
+}
 
 function initialize() {
   bangsPlugin.registerCustomBang({
@@ -164,13 +207,8 @@ function initialize() {
     showSuggestions: function (text, input, event) {
       searchbarPlugins.reset('bangs')
 
-      var isFirst = true
+      var sortLastActivity = searchAndSortTasks(text)
 
-      var sortLastActivity = tasks.map(t => Object.assign({}, { task: t }, { lastActivity: tasks.getLastActivity(t.id) }))
-
-      sortLastActivity = sortLastActivity.sort(function (a, b) {
-        return b.lastActivity - a.lastActivity
-      })
       sortLastActivity.forEach(function (t) {
 
         var task = t.task
@@ -178,17 +216,18 @@ function initialize() {
 
         if (task.id != tasks.getSelected().id) {
           var taskName = (task.name ? task.name : l('defaultTaskName').replace('%n', tasks.getIndex(task.id) + 1))
-          searchbarPlugins.addResult('bangs', {
+
+          var data = {
             title: taskName,
             secondaryText: formatRelativeDate(lastActivity),
-            fakeFocus: isFirst && text,
+            fakeFocus: false,
             click: function () {
               tabEditor.hide()
               moveToTask('%n'.replace('%n', tasks.getIndex(task.id) + 1))
             }
-          })
-          isFirst = false
+          }
 
+          searchbarPlugins.addResult('bangs', data)
         }
       })
     },
@@ -204,32 +243,27 @@ function initialize() {
     showSuggestions: function (text, input, event) {
       searchbarPlugins.reset('bangs')
 
-      var isFirst = true
-
-      var sortLastActivity = tasks.map(t => Object.assign({}, { task: t }, { lastActivity: tasks.getLastActivity(t.id) }))
-
-      sortLastActivity = sortLastActivity.sort(function (a, b) {
-        return b.lastActivity - a.lastActivity
-      })
+      var sortLastActivity = searchAndSortTasks(text)
 
       sortLastActivity.forEach(function (t) {
-        
+
         var task = t.task
         var lastActivity = t.lastActivity
 
         if (task.id != tasks.getSelected().id) {
-
           var taskName = (task.name ? task.name : l('defaultTaskName').replace('%n', tasks.getIndex(task.id) + 1))
-          searchbarPlugins.addResult('bangs', {
+
+          var data = {
             title: taskName,
             secondaryText: formatRelativeDate(lastActivity),
-            fakeFocus: isFirst && text,
+            fakeFocus: false,
             click: function () {
               tabEditor.hide()
               switchToTask('%n'.replace('%n', tasks.getIndex(task.id) + 1))
             }
-          })
-          isFirst = false
+          }
+          
+          searchbarPlugins.addResult('bangs', data)
 
         }
       })
