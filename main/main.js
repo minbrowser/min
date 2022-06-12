ee73e1f930bd69eee42c4af8142bbe30f3e2b067
PalmerAL <PalmerAL@users.noreply.github.com>
2022-06-12 15:26:25 +0200

    Update main.js

--- main/main.js
+++ main/main.js
@@ -86,10 +86,18 @@ var saveWindowBounds = function () {
 }
 
 function sendIPCToWindow (window, action, data) {
-  if (window && window.webContents && window.webContents.isLoading()) {
-    window.webContents.once('did-finish-load', function () {
-      window.webContents.send(action, data || {})
-    })
+  if (window && window.webContents && window.webContents.isLoadingMainFrame()) {
+    // immediately after a did-finish-load event, isLoading can still be true,
+    // so wait a bit to confirm that the page is really loading
+    setTimeout(function() {
+      if (window.webContents.isLoadingMainFrame()) {
+        window.webContents.once('did-finish-load', function () {
+          window.webContents.send(action, data || {})
+        })
+      } else {
+        window.webContents.send(action, data || {})
+      }
+    }, 0)
   } else if (window) {
     window.webContents.send(action, data || {})
   } else {
@@ -206,7 +214,7 @@ function createWindowWithBounds (bounds) {
   if (bounds.maximized) {
     mainWindow.maximize()
 
-    mainWindow.webContents.on('did-finish-load', function () {
+    mainWindow.webContents.once('did-finish-load', function () {
       sendIPCToWindow(mainWindow, 'maximize')
     })
   }
