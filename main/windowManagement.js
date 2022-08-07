8d4b4511865c90bd86cf6c6f0c3c0ce91228a669
PalmerAL <PalmerAL@users.noreply.github.com>
2022-08-07 17:24:01 -0500

    add multiwindow support to main.js and viewManager

---
+++ main/windowManagement.js
@@ -0,0 +1,36 @@
+const windows = {
+  openWindows: [],
+  windowStates: {},
+  windowFromContents: function (webContents) {
+    return windows.openWindows.find(w => w.win.webContents.id === webContents.id).win
+  },
+  addWindow: function (window) {
+    windows.openWindows.push({
+      id: window.id,
+      win: window
+    })
+    windows.windowStates[window.id] = {}
+
+    window.on('focus', function () {
+      windows.getState(window).lastFocused = Date.now()
+    })
+  },
+  removeWindow: function (window) {
+    delete windows.windowStates[window.id]
+    windows.openWindows.splice(windows.openWindows.findIndex(w => w.id === window.id), 1)
+  },
+  getCurrent: function () {
+    const lastFocused = windows.openWindows.sort((a, b) => b.lastFocused - b.lastFocused)[0]
+    if (lastFocused) {
+      return lastFocused.win
+    } else {
+      return null
+    }
+  },
+  getAll: function () {
+    return windows.openWindows.map(w => w.win)
+  },
+  getState: function (window) {
+    return windows.windowStates[window.id]
+  }
+}
