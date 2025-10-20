bab7eab81def8639aac722c863986c3d7d38d30f
Lucas McMurtrie <131608268+Chickaboo@users.noreply.github.com>
2025-10-19 21:25:02 -0500

    Revert "Add customizable homepage"
    This reverts commit 99b360ca071d91eb53f45d9fad6a6b51fdf93a61.

--- main/minInternalProtocol.js
+++ main/minInternalProtocol.js
@@ -19,23 +19,11 @@ function registerBundleProtocol (ses) {
       pathname = pathname.substring(1)
     }
 
-    if (host !== 'app' && host !== 'newtab' && host !== 'blank') {
+    if (host !== 'app') {
       return new Response('bad', {
         status: 400,
         headers: { 'content-type': 'text/html' }
       })
-    }
-
-    // Handle newtab:// protocol
-    if (host === 'newtab') {
-      const newtabPath = path.resolve(__dirname, '../pages/newtab/index.html')
-      return net.fetch(pathToFileURL(newtabPath).toString())
-    }
-
-    // Handle blank:// protocol
-    if (host === 'blank') {
-      const blankPath = path.resolve(__dirname, '../pages/blank/index.html')
-      return net.fetch(pathToFileURL(blankPath).toString())
     }
 
     // NB, this checks for paths that escape the bundle, e.g.
